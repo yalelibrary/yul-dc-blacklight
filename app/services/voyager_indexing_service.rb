9057bbbd51f2ee0d12587aeed55664a0609ba57b
Bess Sadler <bess@curationexperts.com>
2020-04-23 12:36:32 -0400

    Add oid from ladybird to solr records
    Blacklight will need the metadata from voyager plus the oid from ladybird in order to display records properly.

--- app/services/voyager_indexing_service.rb
+++ app/services/voyager_indexing_service.rb
@@ -1,8 +1,10 @@
 require 'json'
 class VoyagerIndexingService
 
-  # Define a location for voyager metadata. We expect the metadata to be a directory of .json files
+  # Define a location for voyager and ladybird metadata.
+  # We expect the metadata to be a directory of .json files
   attr_accessor :voyager_metadata_path
+  attr_accessor :ladybird_metadata_path
 
   # Index a directory of voyager json files
   def index_voyager_metadata
@@ -12,17 +14,45 @@ class VoyagerIndexingService
     end
   end
 
+  def oid_hash
+    @oid_hash ||= build_oid_hash
+  end
+
+  ##
+  # Read in a directory of ladybird json files and create a hash mapping orbisBibId to oid
+  # Blacklight will need the oid in order to fetch the image for display. 
+  def build_oid_hash
+    oid_hash = {}
+    Dir.foreach(ladybird_metadata_path) do |filename|
+      begin
+        next if filename == '.' or filename == '..' or filename.match(/.swp/)
+        file = File.read(File.join(ladybird_metadata_path, filename))
+        data_hash = JSON.parse(file)
+        orbis_bib_id = data_hash["orbisBibId"].to_s
+        oid = data_hash["oid"].to_s
+        oid_hash[orbis_bib_id] = oid
+      # TODO: We need a better way to surface parsing errors
+      # Ideally these would go to an error tracking service that someone would review
+      rescue JSON::ParserError => e
+        puts "JSON::ParserError for #{filename}: #{e}"
+      end
+    end
+    oid_hash
+  end
+
   # Index a single voyager metadata json file
   # @param [String] filename - a full path to a json file
   def index_voyager_json_file(filename)
     file = File.read(File.join(filename))
     data_hash = JSON.parse(file)
+    orbis_bib_id = data_hash["orbisBibId"].to_s
     solr_doc =     {
-      id: data_hash["orbisBibId"],
+      id: orbis_bib_id,
       title_tsim: data_hash["title"],
       language_ssim: data_hash["language"],
       description_tesim: data_hash["description"],
-      author_tsim: data_hash["creator"]
+      author_tsim: data_hash["creator"],
+      oid_ssm: oid_hash[orbis_bib_id]
     }
     solr = Blacklight.default_index.connection
     solr.add([solr_doc])
