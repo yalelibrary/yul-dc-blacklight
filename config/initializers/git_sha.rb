3513a0e53b8fd155926fe9e63e86d165e2211979
k8sewell <kait@notch8.com>
2020-05-19 16:17:52 -0700

    Revise method of reading git sha branch and deployment timestamp

--- config/initializers/git_sha.rb
+++ config/initializers/git_sha.rb
@@ -1,28 +1,42 @@
 # frozen_string_literal: true
-revisions_logfile = "/opt/#{ENV['PROJECT_NAME']}/revisions.log"
 
-sha = `git rev-parse HEAD`.strip
+# note: cmd -  date -u +%FT%TZ
+# note: output - 2020-05-19T23:47:41Z
+
+GITSHA_FILENAME = Rails.root.join('GIT_SHA')
+GITBRANCH_FILENAME = Rails.root.join('GIT_BRANCH')
+DEPLOYEDAT_FILENAME = Rails.root.join('DEPLOYED_AT')
+
 GIT_SHA =
-  if $CHILD_STATUS == 0
-    sha
+  if File.exist?(GITSHA_FILENAME)
+    File.read(GITSHA_FILENAME).strip
   else
-    'Unknown SHA'
+    sha = `git log -1 --pretty=%h`.strip
+    if $CHILD_STATUS == 0
+      sha
+    else
+      'Unknown SHA'
+    end
   end
 
-branch = `git rev-parse --abbrev-ref HEAD`.strip
-BRANCH =
-  if $CHILD_STATUS == 0
-    branch
+GIT_BRANCH =
+  if File.exist?(GITBRANCH_FILENAME)
+    File.read(GITBRANCH_FILENAME).strip
   else
-    'Unknown branch'
+    branch = `git rev-parse --abbrev-ref HEAD`.strip
+    if $CHILD_STATUS == 0
+      branch
+    else
+      'Unknown branch'
+    end
   end
 
 # TODO: Will need updating. Needs to be handled by deployment code
 # Can just dump simple timestamp to be read here
-LAST_DEPLOYED =
-  if Rails.env.production? && File.exist?(revisions_logfile)
-    deployed = `tail -1 #{revisions_logfile}`.chomp.split(" ")[7]
-    Date.parse(deployed).strftime("%d %B %Y")
+DEPLOYED_AT =
+  if File.exist?(DEPLOYEDAT_FILENAME)
+    timestamp = File.read(DEPLOYEDAT_FILENAME).strip
+    Time.iso8601(timestamp).to_formatted_s(:rfc822)
   else
     "Not in deployed environment"
   end
