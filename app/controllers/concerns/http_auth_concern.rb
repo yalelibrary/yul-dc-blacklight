60aed3be7837237ddaaaf96a117dd58f64dbc4b8
Rachel M. Kadel <rachelkg@gmail.com>
2020-05-28 17:40:46 -0400

    rubocop

--- app/controllers/concerns/http_auth_concern.rb
+++ app/controllers/concerns/http_auth_concern.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module HttpAuthConcern
   extend ActiveSupport::Concern
   included do
--- spec/system/password_protect_spec.rb
+++ spec/system/password_protect_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe 'Password protect non-production instances', type: :system do
