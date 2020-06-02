2df7b33badd892cb9e67e69798f333dbd6df4c65
Alisha Evans <alisha@notch8.com>
2020-06-02 12:11:08 -0700

    remove the title label from the individual show page and update tests

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -110,8 +110,6 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field 'title_tsim', label: 'Title'
-    config.add_show_field 'title_vern_ssim', label: 'Title'
     config.add_show_field 'subtitle_tsim', label: 'Subtitle'
     config.add_show_field 'subtitle_vern_ssim', label: 'Subtitle'
     config.add_show_field 'author_tsim', label: 'Author'
