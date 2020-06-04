110b548cd2010a4135bbf71fdba588e69c584e63
Eric DeJesus <eric.dejesus@yale.edu>
2020-06-04 10:03:41 -0400

    194 Added link on format to its facet

--- spec/system/view_fields_in_display_spec.rb
+++ spec/system/view_fields_in_display_spec.rb
@@ -228,5 +228,8 @@ RSpec.feature "View Search Results", type: :system, clean: true, js: false do
     it 'displays the URI in results' do
       expect(document).to have_content("this is the URI")
     end
+    it 'contains a link on format to its facet' do
+      expect(page).to have_link('three dimensional object', href: '/?f%5Bformat%5D%5B%5D=three+dimensional+object')
+    end
   end
 end
