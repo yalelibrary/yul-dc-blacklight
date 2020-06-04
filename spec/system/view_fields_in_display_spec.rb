068fce1532de179c85ca25f917ea2ddfe66ff630
Frederick Rodriguez <>
2020-06-04 12:11:31 -0400

    adds Genre Facet

--- spec/system/view_fields_in_display_spec.rb
+++ spec/system/view_fields_in_display_spec.rb
@@ -67,7 +67,8 @@ RSpec.feature "View Search Results", type: :system, clean: true, js: false do
       digital_ssim: "this is the digital, using ssim",
       coordinates_ssim: "this is the coordinates, using ssim",
       projection_ssim: "this is the projection, using ssim",
-      extent_ssim: "this is the extent, using ssim"
+      extent_ssim: "this is the extent, using ssim",
+      genre_ssim: 'this is the genre'
     }
   end
   context 'Within main document' do
@@ -227,6 +228,9 @@ RSpec.feature "View Search Results", type: :system, clean: true, js: false do
     end
     it 'displays the URI in results' do
       expect(document).to have_content("this is the URI")
+    end
+    it 'contains a link on genre to its facet' do
+      expect(page).to have_link('this is the genre', href: '/?f%5Bgenre_ssim%5D%5B%5D=this+is+the+genre')
     end
     it 'contains a link on format to its facet' do
       expect(page).to have_link('three dimensional object', href: '/?f%5Bformat%5D%5B%5D=three+dimensional+object')
