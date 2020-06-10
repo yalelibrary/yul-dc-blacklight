5de5d172ab94fa6dd382f7e9bf952a56152a761f
Frederick Rodriguez <>
2020-06-10 14:13:54 -0400

    Add date slider facet

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -13,6 +13,7 @@
 #   end
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
+  include BlacklightRangeLimit::RangeLimitBuilder
 
   # Add the `show_only_public_records` method to the processor chain
   self.default_processor_chain += [:show_only_public_records]
