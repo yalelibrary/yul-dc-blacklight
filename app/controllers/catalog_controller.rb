7d5309a21fbd270f66acb26ce6d7df74b6f2b427
Alisha Evans <alisha@notch8.com>
2020-06-10 04:42:18 -0700

    Link the Orbis Bib ID to the Orbis catalog record (#95)
    * the orbis bib id value links to the corresponding catalog record
    * added a test for the Orbis Bib ID link
    * updated the readme concerning (re)building the blacklight image

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -157,7 +157,7 @@ class CatalogController < ApplicationController
     config.add_show_field 'identifierShelfMark_ssim', label: 'Identifier Shelf Mark'
     config.add_show_field 'box_ssim', label: 'Box'
     config.add_show_field 'folder_ssim', label: 'Folder'
-    config.add_show_field 'orbisBibId_ssim', label: 'Orbis Bib ID'
+    config.add_show_field 'orbisBibId_ssim', label: 'Orbis Bib ID', helper_method: :link_to_orbis_bib_id
     config.add_show_field 'orbisBarcode_ssim', label: 'Orbis Bar Code'
     config.add_show_field 'findingAid_ssim', label: 'Finding Aid'
     config.add_show_field 'collectionId_ssim', label: 'Collection ID'
