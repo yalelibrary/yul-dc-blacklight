a21abd6bd5339c58db56ac3595214427ddf9a6f7
jzgo <jez@notch8.com>
2020-06-16 11:16:36 -0700

    Create db when running in production

--- ops/nginx.sh
+++ ops/nginx.sh
@@ -25,7 +25,7 @@ if [[ $PASSENGER_APP_ENV == "production" ]] || [[ $PASSENGER_APP_ENV == "staging
 then
     /bin/bash -l -c 'chown -R app:app /home/app/webapp/tmp' # mounted volume may have wrong permissions
     /bin/bash -l -c 'chown -R app:app /home/app/webapp/public' # mounted volume may have wrong permissions
-    /sbin/setuser app /bin/bash -l -c 'cd /home/app/webapp && bundle exec rake db:migrate'
+    /sbin/setuser app /bin/bash -l -c 'cd /home/app/webapp && bundle exec rake db:create db:migrate'
     if [ -d /home/app/webapp/public/assets-new ]; then
         /sbin/setuser app /bin/bash -l -c 'cd /home/app/webapp && rsync -a public/assets-new/ public/assets/'
     fi
