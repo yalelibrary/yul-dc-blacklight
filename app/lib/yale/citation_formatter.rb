a5845f39f1f0010182c66b9b583b6f47527cbd25
Eric A DeJesus <40175979+edejesusyale@users.noreply.github.com>
2020-06-16 09:53:37 -0400

    Add suggestive MLA and APA citations for records (#114)
    * Add suggestive MLA and APA citations for records
    * Refactor citation modules and add documentation to citation_formatter.rb
    
    Co-authored-by: Alisha Evans <alisha@notch8.com>

---
+++ app/lib/yale/citation_formatter.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+require 'citeproc'
+require 'csl/styles'
+
+module Yale
+  class CitationFormatter
+    include Yale::CitationStringProcessor
+    attr_accessor :obj, :default_citations
+
+    # @param obj [SolrDocument], a document from Solr
+    def initialize(obj)
+      @obj = obj
+      @default_citations = {
+        "apa": apa_default_citation,
+        "modern-language-association": mla_default_citation
+      }
+    end
+
+    # @param style [String], a string containing a style from the @default_citations hash
+    # @return [String], returns the citation for the requested style
+    def citation_for(style)
+      sanitized_citation(CiteProc::Processor.new(style: style, format: 'html').import(item).render(:bibliography, id: :item).first)
+    rescue CiteProc::Error, TypeError, ArgumentError
+      @default_citations[style.to_sym]
+    end
+
+    private
+
+    # @return [CiteProc::Item],a CiteProc::Item instance containing values from the key_value_* chunks
+    def item
+      CiteProc::Item.new(key_value_chunk_1.merge(key_value_chunk_2).merge(key_value_chunk_3))
+    end
+
+    # @return [Boolean],returns true if author_ssim has a character that not in any language, else false
+    def abnormal_chars?
+      obj[:author_ssim]&.any? { |a| a.match(/[^\p{L}\s]+/) }
+    end
+
+    # @return [Hash], a hash with a Solr Document's fields and values as key,value pairs
+    def key_value_chunk_1
+      {
+        id: :item,
+        abstract: obj[:abstract_ssim]&.join(', '),
+        archive_location: obj[:sublocation_tesim]&.join(', '),
+        author: obj[:author_ssim]&.join(', '),
+        "call-number": obj[:identifierShelfMark_ssim]&.join(', '),
+        edition: obj[:edition_tesim]&.join(', '),
+        institution: obj[:institution_tesim]&.join(', ')
+      }
+    end
+
+    # @return [Hash], a hash with a Solr Document's fields and values as key,value pairs
+    def key_value_chunk_2
+      {
+        archive: obj[:holding_repository_tesim]&.join(', '),
+        publisher: obj[:publisher_ssim]&.join(', '),
+        title: obj[:title_tsim]&.join(', '),
+        "collection-title": obj[:member_of_collections_ssim]&.join(', '),
+        type: [obj[:format]&.first&.downcase]&.join(', '),
+        url: url,
+        issued: (obj[:date_tsim] || obj[:year_issued_isim] || obj[:year_created_isim] || [0])&.first&.to_i
+      }
+    end
+
+    # @return [Hash], a hash with a Solr Document's fields and values as key,value pairs
+    def key_value_chunk_3
+      {
+        dimensions: obj[:extent_ssim]&.join(', '),
+        event: obj[:conference_name_tesim]&.join(', '),
+        genre: obj[:genre_ssim]&.join(', '),
+        ISBN: obj[:isbn_tesim]&.join(', '),
+        ISSN: obj[:issn_tesim]&.join(', '),
+        keyword: obj[:keywords_tesim]&.join(', '),
+        "publisher-place": obj[:publicationPlace_ssim]&.join(', ')
+      }
+    end
+  end
+end
---
+++ spec/helpers/citation_helper_spec.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.feature "Citation Helper", clean: true, system: true do
+  before do
+    solr = Blacklight.default_index.connection
+    solr.add([test_record])
+    solr.commit
+    visit '/catalog/111'
+  end
+
+  let(:test_record) do
+    {
+      id: '111',
+      subtitle_tsim: "He's handsome",
+      subtitle_vern_ssim: "He's handsome",
+      author_tsim: 'Eric & Frederick',
+      author_ssim: 'Eric & Frederick',
+      format: 'three dimensional object',
+      url_fulltext_ssim: 'http://0.0.0.0:3000/catalog/111',
+      url_suppl_ssim: 'http://0.0.0.0:3000/catalog/111',
+      published_ssim: "1997",
+      published_vern_ssim: "1997",
+      lc_callnum_ssim: "123213213",
+      language_ssim: ['en', 'eng', 'zz'],
+      isbn_ssim: '2321321389',
+      description_tesim: "Handsome Dan is a bulldog who serves as Yale Univeristy's mascot.",
+      visibility_ssi: 'Public',
+      abstract_ssim: "this is an abstract",
+      alternativeTitle_ssim: "this is an alternative title",
+      genre_ssim: "this is the genre",
+      geoSubject_ssim: "this is the geo subject",
+      resourceType_ssim: "this is the resource type",
+      subjectName_ssim: "this is the subject name",
+      subjectTopic_ssim: "this is the subject topic",
+      extentOfDigitization_ssim: 'this is the extent of digitization',
+      rights_ssim: "these are the rights",
+      publicationPlace_ssim: "this is the publication place",
+      sourceCreated_ssim: "this is the source created",
+      publisher_ssim: "this is the publisher",
+      copyrightDate_ssim: "this is the copyright date",
+      source_ssim: "this is the source",
+      recordType_ssim: "this is the record type",
+      sourceTitle_ssim: "this is the source title",
+      sourceDate_ssim: "this is the source date",
+      sourceNote_ssim: "this is the source note",
+      references_ssim: "these are the references",
+      date_tsim: "this is the date",
+      children_ssim: "these are the children",
+      importUrl_ssim: "this is the import URL",
+      illustrativeMatter_ssim: "this is the illustrative matter",
+      oid_ssim: 'this is the OID',
+      identifierMfhd_ssim: 'this is the identifier MFHD',
+      identifierShelfMark_ssim: 'this is the identifier shelf mark',
+      box_ssim: 'this is the box',
+      folder_ssim: 'this is the folder',
+      orbisBibId_ssim: 'this is the orbis bib ID',
+      orbisBarcode_ssim: 'this is the orbis bar code',
+      findingAid_ssim: 'this is the finding aid',
+      collectionId_ssim: 'this is the collection ID',
+      edition_ssim: 'this is the edition',
+      uri_ssim: 'this is the URI',
+      partOf_ssim: "this is the part of, using ssim",
+      numberOfPages_ssim: "this is the number of pages, using ssim",
+      material_ssim: "this is the material, using ssim",
+      scale_ssim: "this is the scale, using ssim",
+      digital_ssim: "this is the digital, using ssim",
+      coordinates_ssim: "this is the coordinates, using ssim",
+      projection_ssim: "this is the projection, using ssim",
+      extent_ssim: "this is the extent, using ssim"
+    }
+  end
+
+  context 'Clicking on cite' do
+    it 'displays correct MLA citation' do
+      click_on "Cite"
+      expect(page).to have_css('#mla-citation')
+      within('#mla-citation') do
+        expect(page).to have_content("Eric, and Frederick. this is the publisher, 0AD. http://collections-demo.curationexperts.com/catalog/111.")
+      end
+    end
+    it 'displays correct APA citation' do
+      click_on "Cite"
+      expect(page).to have_css('#mla-citation')
+      within('#apa-citation') do
+        expect(page).to have_content("E., & F. (0 C.E.). [This is the genre]. this is the publisher. http://collections-demo.curationexperts.com/catalog/111.")
+      end
+    end
+  end
+end
