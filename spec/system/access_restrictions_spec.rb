3fab8d068d4f1bc83661bf48b2fd8a6ec30c67d1
Frederick Rodriguez <>
2020-06-18 16:18:35 -0400

    Authenticate users against Yale CAS
    Co-authored-by: Eric DeJesus <eric.dejesus@yale.com>

--- spec/system/access_restrictions_spec.rb
+++ spec/system/access_restrictions_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe "access restrictions", type: :system, clean: true do
   # "[Map of China]. [yale-only copy]"
   let(:yale_work) { WORK_WITH_YALE_ONLY_VISIBILITY }
 
+  let(:cas) {WORK_WITH_YALE_ONLY_VISIBILITY}
+
   before do
     solr = Blacklight.default_index.connection
     solr.add(public_work)
@@ -42,6 +44,30 @@ RSpec.describe "access restrictions", type: :system, clean: true do
   end
 
   context "an authenticated user" do
+    before do
+      login_as user
+    end
+
+    it 'displays public and yale-only but NOT private works in search results' do
+      visit '/?search_field=all_fields&q='
+
+      expect(page).to have_content('A General dictionary of the English language')
+      expect(page).to have_content('[Map of China]. [yale-only copy]')
+      expect(page).not_to have_content('[Map of China]. [private copy]')
+    end
+
+    it "displays universal viewer for public works" do
+      visit solr_document_path(public_work[:id])
+      expect(page.html).to match(/universal-viewer-iframe/)
+    end
+
+    it "displays universal viewer for yale-only works" do
+      visit solr_document_path(public_work[:id])
+      expect(page.html).to match(/universal-viewer-iframe/)
+    end
+  end
+
+  context "an CAS authenticated user" do
     before do
       login_as user
     end
