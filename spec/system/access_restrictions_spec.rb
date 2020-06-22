f04674fcaeece2f56fdb2f11e1fd1ae7dc2599ea
Eric DeJesus <eric.dejesus@yale.edu>
2020-06-22 12:54:36 -0400

    Remove redundant tests

--- spec/system/access_restrictions_spec.rb
+++ spec/system/access_restrictions_spec.rb
@@ -66,28 +66,4 @@ RSpec.describe "access restrictions", type: :system, clean: true do
       expect(page.html).to match(/universal-viewer-iframe/)
     end
   end
-
-  context "an CAS authenticated user" do
-    before do
-      login_as user
-    end
-
-    it 'displays public and yale-only but NOT private works in search results' do
-      visit '/?search_field=all_fields&q='
-
-      expect(page).to have_content('A General dictionary of the English language')
-      expect(page).to have_content('[Map of China]. [yale-only copy]')
-      expect(page).not_to have_content('[Map of China]. [private copy]')
-    end
-
-    it "displays universal viewer for public works" do
-      visit solr_document_path(public_work[:id])
-      expect(page.html).to match(/universal-viewer-iframe/)
-    end
-
-    it "displays universal viewer for yale-only works" do
-      visit solr_document_path(public_work[:id])
-      expect(page.html).to match(/universal-viewer-iframe/)
-    end
-  end
 end
