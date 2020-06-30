83e8efe476a600e6f546e02f5f0435ad34ba52e8
Alisha Evans <alisha@notch8.com>
2020-06-30 12:45:13 -0700

    Add specs for search_history_controller.rb (#152)
    * install the rails-controller-testing gem
    * added specs for the search history controller

---
+++ spec/controllers/search_history_controller_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+RSpec.describe SearchHistoryController do
+  routes { Blacklight::Engine.routes }
+
+  describe 'index' do
+    let(:one) { Search.create }
+    let(:two) { Search.create }
+    let(:three) { Search.create }
+
+    it 'only fetches searches with ids in the session' do
+      session[:history] = [one.id, three.id]
+      get :index
+      searches = assigns(:searches)
+      expect(searches).to include(one)
+      expect(searches).not_to include(two)
+    end
+
+    it 'tolerates bad ids in session' do
+      session[:history] = [one.id, three.id, 'NOT_IN_DB']
+      get :index
+      searches = assigns(:searches)
+      expect(searches).to include(one)
+      expect(searches).to include(three)
+    end
+
+    it 'does not fetch any searches if there is no history' do
+      session[:history] = []
+      get :index
+      searches = assigns(:searches)
+      expect(searches).to be_empty
+    end
+  end
+end
