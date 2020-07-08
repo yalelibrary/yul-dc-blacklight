3a874de5766988ad583a3095d69f3d0c1c588923
Frederick Rodriguez <>
2020-07-08 15:49:14 -0400

    Add Test for basic http status codes 401 and 200
    Co-authored-by: Eric DeJesus <eric.dejesus@yale.com>

---
+++ spec/controllers/http_auth_concern_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'net/http'
+
+RSpec.describe HttpAuthConcern, type: :request do
+
+  before do
+    @controller = controller
+    ENV['HTTP_PASSWORD_PROTECT'] = true.to_s
+  end
+  after do
+    ENV['HTTP_PASSWORD_PROTECT'] = false.to_s
+  end
+
+  it "protects sensitive pages" do
+    get '/'
+    response.status.should == 401
+  end
+
+  it 'after sign-in user has access' do
+  http_auth = http_login
+  get '/', headers: { 'HTTP_AUTHORIZATION' => http_auth }
+    response.status.should == 200
+  end
+end
