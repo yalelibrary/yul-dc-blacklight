386f82f7a25f129195aaaf87d1dcc29b81de2c22
Frederick Rodriguez <>
2020-07-14 11:18:29 -0400

    added pending block to accessibility test code

--- spec/user_views_accessible_welcome_page_spec.rb
+++ spec/user_views_accessible_welcome_page_spec.rb
@@ -5,6 +5,7 @@ require 'axe/rspec'
 
 feature 'welcome', js: true do
   scenario 'index is accessible' do
+    pending 'prototype working axe gem on landing page'
     visit "/"
     expect(page).to be_accessible
   end
