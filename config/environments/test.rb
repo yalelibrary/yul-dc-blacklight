9b79fcfea2ac124d95fb7a90e5fc3fa209e1ddc8
McClain Looney <m@loonsoft.com>
2020-07-23 11:36:27 -0500

    tune down test log output

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.log_level = :error
 
   config.cache_classes = false
 
