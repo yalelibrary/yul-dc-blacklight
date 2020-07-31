6a2df4a6f8109fbc019448f3e8f0524d756e5402
Frederick Rodriguez <>
2020-07-31 13:59:04 -0400

    Update main search behavior

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -202,7 +202,21 @@ class CatalogController < ApplicationController
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
     # since we aren't specifying it otherwise.
 
-    config.add_search_field 'all_fields', label: 'All Fields'
+    # Blacklight 'out of box code'
+    # config.add_search_field 'all_fields', label: 'All Fields'
+
+    # Array allows for only listed Solr fields to be searched in the 'All Fields'
+    search_fields = ['abstract_ssim', 'alternativeTitle_ssim', 'orbisBidId_ssim', 'description_tesim',
+                     'publicatonPlace_ssim', 'publisher_ssim', 'sourceCreated_ssim', 'geo_subject_ssim',
+                     'subjectName_ssim', 'subject_topic_tsim', 'title_tsim']
+
+    config.add_search_field('all_fields', label: 'All Fields') do |field|
+      field.qt = 'search'
+      field.solr_parameters = {
+        qf: search_fields,
+        pf: ''
+      }
+    end
 
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
@@ -237,8 +251,8 @@ class CatalogController < ApplicationController
     config.add_search_field('orbisBibId_ssim', label: 'BibID') do |field|
       field.qt = 'search'
       field.solr_parameters = {
-        qf: 'orbisBibId_ssim',
-        pf: ''
+        qf: '',
+        pf: 'orbisBibId_ssim'
       }
     end
 
