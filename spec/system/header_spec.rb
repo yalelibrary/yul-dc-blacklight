c57360f52809321d363b3843101407dac1be789e
Frederick Rodriguez <>
2020-08-03 09:52:44 -0400

    Added RSpec test

--- spec/system/header_spec.rb
+++ spec/system/header_spec.rb
@@ -7,10 +7,15 @@ RSpec.describe 'header', type: :system do
   end
 
   it 'has expected css' do
-    expect(page).to have_css '.nav-link'
     expect(page).to have_css '.bg-dark'
-    expect(page).to have_css '.navbar-logo'
+    expect(page).to have_css '.dropdown-menu'
+    expect(page).to have_css '.info-header'
+    expect(page).to have_css '.nav-link'
     expect(page).to have_css '.navbar-brand'
+    expect(page).to have_css '.navbar-logo'
+    expect(page).to have_css '.secondary-nav'
+    expect(page).to have_css 'div#secondary-nav row'
+    expect(page).to have_css '.secondary-nav a'
   end
 
   it 'has expected links' do
@@ -21,6 +26,6 @@ RSpec.describe 'header', type: :system do
   end
 
   it 'has expected Yale log branding' do
-    expect(page.find('.navbar-logo')['//app/images/yul_log/'])
+    expect(page.find('.navbar-logo')['//app/images/yul_logo/'])
   end
 end
