0de702bbc16e14d5daab181a58a586a272d0b740
Frederick Rodriguez <>
2020-08-04 09:14:16 -0400

    Fixed RSpec test

--- spec/system/highlight_in_search_spec.rb
+++ spec/system/highlight_in_search_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe 'Search results displays field', type: :system, clean: true do
     end
 
     it 'highlights the call number when a term is queried' do
-      visit '/?search_field=all_fields&q=music'
+      visit '/?search_field=all_fields&q=Music'
       expect(page.html).to include "Osborn <span class='search-highlight'>Music</span> MS 4"
     end
   end
