4ce7428f87145709b8c93f271fce19dcbb50dc77
Frederick Rodriguez <>
2020-08-11 16:07:13 -0400

    Removed bookmark checkbox on research and item page

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -34,13 +34,15 @@ class CatalogController < ApplicationController
     # config.index.display_type_field = 'format'
     config.index.thumbnail_method = :render_thumbnail
 
-    config.add_results_document_tool(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
+    # Remove bookmark functionality from the research page
+    # config.add_results_document_tool(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
 
     config.add_results_collection_tool(:sort_widget)
     config.add_results_collection_tool(:per_page_widget)
     config.add_results_collection_tool(:view_type_group)
 
-    config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
+    # Remove bookmark functionality from the item page
+    # config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
     config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
     config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
     config.add_show_tools_partial(:citation)
