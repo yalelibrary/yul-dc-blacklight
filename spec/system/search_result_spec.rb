0b6e620877cdd9ccdb63eff7c469829afeea4f2f
Frederick Rodriguez <41123693+FCRodriguez7@users.noreply.github.com>
2020-08-13 16:46:52 -0400

    Remove excess whitespace in Safari (#232)
    * Remove excess whitespace in Safari
    
    * Fixed text bleed on image
    
    * Added rspec test
    
    * Added code style
    
    * Added the appropriate styling for mobile devices
    
    * Added the appropriate styling for larger screen size devices
    
    * Added the appropriate styling for medium screen size devices and code testing
    
    * Added the appropriate styling for another screen size devices
    
    Co-authored-by: Frederick Rodriguez <>

---
+++ spec/system/search_result_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe 'search result', type: :system do
+  before do
+    visit '/?search_field=all_fields&q='
+  end
+
+  it 'has expected css' do
+    expect(page).to have_css '.dl-invert'
+    expect(page).to have_css '.document-metadata'
+  end
+end
