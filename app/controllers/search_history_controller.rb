e4fc3cf62496afbda169a73b62b5a5f752c1f374
Frederick Rodriguez <41123693+FCRodriguez7@users.noreply.github.com>
2020-09-09 13:31:01 -0400

    Fix bug for deployment of code v1.16.0 (#267)

--- app/controllers/search_history_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-class SearchHistoryController < ApplicationController
-  include Blacklight::SearchHistory
-
-  helper BlacklightRangeLimit::ViewHelperOverride
-  # helper BlacklightAdvancedSearch::RenderConstraintsOverride
-  helper RangeLimitHelper
-  helper BlacklightAdvancedSearch::RenderConstraintsOverride
-end
