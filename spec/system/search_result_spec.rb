6a79a7a11f4b57136dd162c38ea67dceb3874b23
Alisha Evans <alisha@notch8.com>
2020-09-11 09:44:19 -0700

    Style gallery view search results (#273)
    * remove leftover comment
    
    * xsmall device styled
    
    * finished the media queries
    
    * added a test for the gallery view

--- spec/system/search_result_spec.rb
+++ spec/system/search_result_spec.rb
@@ -2,12 +2,25 @@
 require 'rails_helper'
 
 RSpec.describe 'search result', type: :system do
-  before do
-    visit '/?search_field=all_fields&q='
+  context 'in list view' do
+    before do
+      visit '/?search_field=all_fields&view=list'
+    end
+
+    it 'has expected css' do
+      expect(page).to have_css '.dl-invert'
+      expect(page).to have_css '.document-metadata'
+    end
   end
 
-  it 'has expected css' do
-    expect(page).to have_css '.dl-invert'
-    expect(page).to have_css '.document-metadata'
+  context 'in gallery view' do
+    before do
+      visit '/?q=&search_field=all_fields&view=gallery'
+    end
+
+    it 'has expected css' do
+      expect(page).to have_css '.index'
+      expect(page).to have_css '.caption'
+    end
   end
 end
