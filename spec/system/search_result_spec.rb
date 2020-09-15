134ad616abbe34b0c746f5b36163e54a0330fb7d
Alisha Evans <alisha@notch8.com>
2020-09-15 16:59:23 -0700

    added specs for the "per page" options

--- spec/system/search_result_spec.rb
+++ spec/system/search_result_spec.rb
@@ -11,7 +11,10 @@ RSpec.describe 'search result', type: :system do
       expect(page).to have_css '.dl-invert'
       expect(page).to have_css '.document-metadata'
     end
-  end
+
+    it 'has the correct "per page" options' do
+      expect(page).to have_text '10 per page 20 per page 50 per page 100 per page'
+    end
 
   context 'in gallery view' do
     before do
@@ -19,8 +22,11 @@ RSpec.describe 'search result', type: :system do
     end
 
     it 'has expected css' do
-      expect(page).to have_css '.index'
       expect(page).to have_css '.caption'
+    end
+
+    it 'has the correct "per page" options' do
+      expect(page).to have_text '9 per page 30 per page 60 per page 99 per page'
     end
   end
 end
