4f54bb11c5c96155a7d18009798ff58be2f5eeb2
Alisha Evans <alisha@notch8.com>
2020-09-16 11:38:05 -0700

    Removed index numbers from the gallery view only (#283)

--- spec/system/search_result_spec.rb
+++ spec/system/search_result_spec.rb
@@ -15,6 +15,10 @@ RSpec.describe 'search result', type: :system do
     it 'has the correct "per page" options' do
       expect(page).to have_text '10 per page 20 per page 50 per page 100 per page'
     end
+
+    it 'shows the index number' do
+      expect(page).to have_selector '#documents > .document-position-0 span', visible: true
+    end
   end
 
   context 'in gallery view' do
@@ -28,6 +32,10 @@ RSpec.describe 'search result', type: :system do
 
     it 'has the correct "per page" options' do
       expect(page).to have_text '9 per page 30 per page 60 per page 99 per page'
+    end
+
+    it 'does not show the index number' do
+      expect(page).to have_selector '#documents > .document.col:first-child span', visible: false
     end
   end
 end
