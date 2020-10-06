cbc9f22a71056e343f5969b0708223116802d2cf
Alisha Evans <alisha@notch8.com>
2020-10-06 12:40:45 -0700

    create the generic metadata presenter class

---
+++ app/presenters/yul/metadata_presenter.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Yul
+  class MetadataPresenter
+    attr_reader :document, :config
+
+    def initialize(document:, name:)
+      @document = document.detect { |_i| Blacklight::ShowPresenter }[2].instance_variable_get("@document").to_h
+      @config = YAML.safe_load(File.open(Rails.root.join('config', 'metadata', "#{name}_metadata.yml")))
+    end
+
+    def terms
+      @document.slice(*@config.keys)
+    end
+  end
+end
