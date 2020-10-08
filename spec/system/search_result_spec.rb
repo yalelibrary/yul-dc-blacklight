8a212d95be9ee23b41866772262e31c5f1fc756d
FCRodriguez7 <frederick.rodriguez@yale.edu>
2020-10-08 16:34:31 -0400

    Completed the Rspec

--- spec/system/search_result_spec.rb
+++ spec/system/search_result_spec.rb
@@ -1,15 +1,46 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe 'search result', type: :system do
+RSpec.describe 'search result', type: :system, clean: true do
+  before do
+    solr = Blacklight.default_index.connection
+    solr.add(document_with_image)
+    solr.commit
+    stub_request(:get, "http://iiif_image:8182/iiif/2/1234822/full/!200,200/0/default.jpg")
+      .to_return(status: 200, body: File.open("spec/fixtures/images/Sun.png").read, headers: { "Content-Type" => /image\/.+/ })
+  end
+
+  let(:document_with_image) do
+    {
+      id: 'test_record_1',
+      oid_ssi: '2055095',
+      visibility_ssi: 'Public',
+      identifierShelfMark_tesim: 'Call Number',
+      thumbnail_path_ss: "http://iiif_image:8182/iiif/2/1234822/full/!200,200/0/default.jpg"
+    }
+  end
+
   context 'in list view' do
     before do
       visit '/?search_field=all_fields&view=list'
     end
-
-    it 'has expected css' do
-      expect(page).to have_css '.dl-invert'
-      expect(page).to have_css '.document-metadata'
+    it 'has expected css', js: true, style: true do
+      within '.documents-list' do
+        expect(page).to have_css '.dl-invert'
+        expect(page).to have_css '.document-metadata'
+        expect(page).to have_css '.document-title'
+        expect(page).to have_css '.document-title-heading'
+        expect(page).to have_css '.documentHeader'
+        expect(page).to have_css '.row'
+        expect(page).to have_css '.document'
+        expect(page).to have_css '.document-metadata.dl-invert.row'
+        expect(page).to have_css '#documents > article.document > dl > dt'
+        expect(page).to have_css '.document-metadata.dl-invert > dt'
+        expect(page).to have_css '.document-metadata.dl-invert > dd'
+        expect(page).to have_css '.document-metadata'
+        expect(page).to have_css '.document-thumbnail'
+        expect(page).to have_css '.col-md-9'
+      end
     end
 
     it 'has the correct "per page" options' do
@@ -24,10 +55,6 @@ RSpec.describe 'search result', type: :system do
   context 'in gallery view' do
     before do
       visit '/?q=&search_field=all_fields&view=gallery'
-    end
-
-    it 'has expected css' do
-      expect(page).to have_css '.caption'
     end
 
     it 'has the correct "per page" options' do
