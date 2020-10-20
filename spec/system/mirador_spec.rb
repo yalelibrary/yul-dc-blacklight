3e246d2c5eeccaf7eb3be099617006e6ef498ef5
mikeapp <michael.appleby@yale.edu>
2020-10-20 07:01:45 -0400

    Test for Mirador configuration

---
+++ spec/system/mirador_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe 'Mirador viewer', type: :system do
+  context 'Configure Mirador' do
+    it 'configures a manifest given an integer value' do
+      value = 123_456
+      visit "/mirador/#{value}"
+      expect(page.html).to match(/const manifest =.*#{value}/)
+    end
+
+    it 'builds a blank configuration given an invalid value' do
+      value = '1_malicious_stuff_here'
+      visit "/mirador/#{value}"
+      expect(page.html).not_to match(/const manifest/)
+      expect(page.html).not_to include value
+    end
+  end
+end
