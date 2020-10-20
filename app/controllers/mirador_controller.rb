cefeca4653c4db2d51f614cd11022c8309c6f193
mikeapp <michael.appleby@yale.edu>
2020-10-19 20:13:46 -0400

    Minimal input validation; display empty Mirador if no OID provided

--- app/controllers/mirador_controller.rb
+++ app/controllers/mirador_controller.rb
@@ -1,8 +1,17 @@
 class MiradorController < ApplicationController
 
+  include BlacklightHelper
+
   def show
-    @oid = params[:oid]
+    @oid = number_or_nil params[:oid]
+    @manifest = @oid ? manifest_url(@oid) : nil
     render layout: false
+  end
+
+  def number_or_nil(string)
+    Integer(string || '')
+  rescue ArgumentError
+    nil
   end
 
 end
