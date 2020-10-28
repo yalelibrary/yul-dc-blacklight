c3cc9a32a03d98edde19cace32fc42c91373052c
Frederick Rodriguez <41123693+FCRodriguez7@users.noreply.github.com>
2020-10-28 17:39:36 -0400

    Revert "Fix date slider bounds and tests. Add date constraint. (#357)"
    This reverts commit fc2a8c3e5e3f1517b26777813ba1e2b2af4ba64c.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -29,16 +29,3 @@ RSpec.configure do |config|
     driven_by :chrome_headless
   end
 end
-
-module CapybaraExtension
-  delegate :drag_by, to: :base
-end
-
-module CapybaraSeleniumExtension
-  def drag_by(right_by, down_by)
-    driver.browser.action.drag_and_drop_by(native, right_by, down_by).perform
-  end
-end
-
-::Capybara::Selenium::Node.send :include, CapybaraSeleniumExtension
-::Capybara::Node::Element.send :include, CapybaraExtension
