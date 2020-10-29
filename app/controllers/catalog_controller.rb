87187e3307ad8c287c1460372d22fc84c2d296f3
Lakeisha Robinson <lakeisharobinson@vpn172022119159.its.yale.internal>
2020-10-29 15:56:52 -0400

    Add Ascending and Descending options for result sorting#730
    Co-authored-by: Martin Lovell <martin.lovell@yale.edu>
    Co-authored-by: Frederick Rodriguez <frederick.rodriguez@yale.edu>
    Co-authored-by: Maggie Zhao <maggie.zhao@yale.edu>
    Co-authored-by: Eric DeJesus <eric.dejesus@yale.edu>

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -442,8 +442,10 @@ class CatalogController < ApplicationController
     # config.add_sort_field 'pub_date_si desc, title_si asc', label: 'year'
     # config.add_sort_field 'dateStructured_ssim asc, title_si asc', label: 'date (oldest first)'
     # config.add_sort_field 'dateStructured_ssim desc, title_si asc', label: 'date (newest first)'
-    config.add_sort_field 'creator_ssim asc, title_ssim asc', label: 'creator'
-    config.add_sort_field 'title_ssim asc, pub_date_si desc', label: 'title'
+    config.add_sort_field 'creator_ssim asc, title_ssim asc', label: 'Creator (A --> Z)'
+    config.add_sort_field 'creator_ssim desc, title_ssim desc', label: 'Creator (Z --> A)'
+    config.add_sort_field 'title_ssim asc, pub_date_si desc', label: 'Title (A --> Z)'
+    config.add_sort_field 'title_ssim desc, pub_date_si desc', label: 'Title (Z --> A)'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
