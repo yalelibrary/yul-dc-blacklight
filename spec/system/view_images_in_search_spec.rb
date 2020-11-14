47857b61f8e5ceca7b63436d14461b158e5fdcf7
mikeapp <michael.appleby@yale.edu>
2020-11-13 21:18:48 -0500

    Move check and display of image_not_found placeholder to client side

--- spec/system/view_images_in_search_spec.rb
+++ spec/system/view_images_in_search_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe 'Search results displays images', type: :system, clean: true, js:
     solr = Blacklight.default_index.connection
     solr.add([document_with_image,
               document_without_image,
-              document_yale_only_image])
+              document_yale_only_image,
+              document_with_broken_image])
     solr.commit
     stub_request(:get, "http://iiif_image:8182/iiif/2/1234822/full/!200,200/0/default.jpg")
-      .to_return(status: 200, body: File.open("spec/fixtures/images/Sun.png").read, headers: { "Content-Type" => /image\/.+/ })
+      .to_return(status: 200, body: File.open("spec/fixtures/images/small_jpeg.jpeg").read)
   end
 
   let(:document_with_image) do
@@ -35,8 +36,16 @@ RSpec.describe 'Search results displays images', type: :system, clean: true, js:
       visibility_ssi: 'Public'
     }
   end
+  let(:document_with_broken_image) do
+    {
+      id: 'test_record_4',
+      oid_ssi: '99',
+      visibility_ssi: 'Public',
+      thumbnail_path_ss: "http://iiif_image:8182/iiif/2/foo"
+    }
+  end
 
-  context 'public records with images', style: true do
+  context 'public records with images', style: true, js: false do
     it 'displays thumbnail for oids with images' do
       visit '?q=&search_field=all_fields'
       expect(page).to have_xpath("//img[@src = 'http://iiif_image:8182/iiif/2/1234822/full/!200,200/0/default.jpg']")
@@ -45,6 +54,13 @@ RSpec.describe 'Search results displays images', type: :system, clean: true, js:
     it 'displays the image_not_found.png for records without images' do
       visit '?q=&search_field=all_fields'
       expect(page).to have_css("img[src ^= '/assets/image_not_found']")
+    end
+  end
+
+  context 'public records with broken images', style: true do
+    it 'displays the image_not_found_png for records with broken images' do
+      visit "?q=#{document_with_broken_image[:oid_ssi]}&search_field=oid_ssi"
+      expect(page.html).to match('/assets/image_not_found')
     end
   end
 
@@ -66,7 +82,7 @@ RSpec.describe 'Search results displays images', type: :system, clean: true, js:
     end
 
     context 'as a logged in user' do
-      it 'displays the image', style: true do
+      it 'displays the image', style: true, js: false do
         user = FactoryBot.create(:user)
         login_as(user, scope: :user)
 
