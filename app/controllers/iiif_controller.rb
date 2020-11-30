ed80cab86adf5ab39eea4801dabc9c455e68003e
Rob Kaufman <rob@notch8.com>
2020-11-30 09:16:44 -0800

    rubocop

--- app/controllers/iiif_controller.rb
+++ app/controllers/iiif_controller.rb
@@ -14,10 +14,10 @@ class IiifController < ApplicationController
   # IIIF doesn't just return the oid, find the child, then find the oid from there
   def search_for_item
     child_oid = params[:id].gsub(/^2\/(\d+)\/.*/, '\1')
-    search_state[:q] = {child_oids_ssim: child_oid}
+    search_state[:q] = { child_oids_ssim: child_oid }
     search_state[:rows] = 1
     search_service_class.new(config: blacklight_config, search_state: search_state, user_params: search_state.to_h, **search_service_context)
     r, d = search_service.search_results
-    return [r, d.first]
+    [r, d.first]
   end
 end
