2da783432e9712ad73f888efbb031572e69f2d6a
martinlovell <32851993+martinlovell@users.noreply.github.com>
2020-12-01 14:00:34 -0500

    check authorization for PDFs (#416)
    * check authorization for PDFs
    
    * Check visibility for PDFs
    
    * Return unauthorized rather than not found on unauthorized pdf request
    
    * Clarify with better titles for test works
    
    * Use CheckAuthorization for manifests, return 401 when no visibility is set

--- app/controllers/manifests_controller.rb
+++ app/controllers/manifests_controller.rb
@@ -2,7 +2,7 @@
 
 # Takes a request for a manifest/oid and stream the JSON for that oid from S3
 class ManifestsController < ApplicationController
-  before_action :check_authorization
+  include CheckAuthorization
   include Blacklight::Catalog
 
   def show
@@ -24,25 +24,5 @@ class ManifestsController < ApplicationController
     client = Aws::S3::Client.new
     response = client.get_object(bucket: ENV['SAMPLE_BUCKET'], key: remote_path)
     response.body&.read
-  end
-
-  def check_authorization
-    @response, @document = search_service.fetch(params[:id])
-
-    # Handle when the 'visibility_ssi' key doesn't exist on the manifest
-    unless @document.key?('visibility_ssi')
-      render json: { error: 'not-found' }.to_json, status: 404
-      return false
-    end
-
-    case @document['visibility_ssi']
-    when 'Public'
-      true
-    when 'Yale Community Only'
-      return true if current_user
-
-      render json: { error: 'unauthorized' }.to_json, status: 401
-      false
-    end
   end
 end
