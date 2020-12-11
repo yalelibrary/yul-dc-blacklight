c3cd6406f053af196b1759faac3156a35efcb851
Eric DeJesus <eric.dejesus@yale.edu>
2020-12-11 09:17:04 -0500

    Test years with less than 4 digits

--- spec/system/facet_by_year_spec.rb
+++ spec/system/facet_by_year_spec.rb
@@ -4,12 +4,29 @@ require 'rails_helper'
 RSpec.feature "View Search Results", type: :system, clean: true, js: false do
   before do
     solr = Blacklight.default_index.connection
-    solr.add([test_record_0, test_record_1, test_record_2])
+    solr.add([test_record_0, test_record_1, test_record_2, test_record_3])
     solr.commit
     # visit '/catalog/211'
   end
 
   let(:test_record_0) do
+    {
+      id: '111',
+      subtitle_tesim: "He's Handsome",
+      creator_tesim: 'Eric & Frederick',
+      format: 'three dimensional object',
+      url_fulltext_ssim: 'http://0.0.0.0:3000/catalog/211',
+      url_suppl_ssim: 'http://0.0.0.0:3000/catalog/211',
+      published_ssim: "1997",
+      language_ssim: ['en', 'eng', 'zz'],
+      isbn_ssim: '2321321389',
+      description_tesim: "Handsome Dan is a bulldog who serves as Yale Univeristy's mascot.",
+      visibility_ssi: 'Public',
+      year_isim: (600..1050).to_a
+    }
+  end
+
+  let(:test_record_1) do
     {
       id: '211',
       subtitle_tesim: "He's Handsome",
@@ -26,7 +43,7 @@ RSpec.feature "View Search Results", type: :system, clean: true, js: false do
     }
   end
 
-  let(:test_record_1) do
+  let(:test_record_2) do
     {
       id: '311',
       subtitle_tesim: "He's Dan",
@@ -43,7 +60,7 @@ RSpec.feature "View Search Results", type: :system, clean: true, js: false do
     }
   end
 
-  let(:test_record_2) do
+  let(:test_record_3) do
     {
       id: '411',
       subtitle_tesim: "He's Handsome Dan",
@@ -65,6 +82,7 @@ RSpec.feature "View Search Results", type: :system, clean: true, js: false do
     click_on 'search'
 
     within '#documents' do
+      expect(page).to have_content("111")
       expect(page).to have_content("211")
       expect(page).to have_content("311")
       expect(page).to have_content("411")
@@ -76,6 +94,31 @@ RSpec.feature "View Search Results", type: :system, clean: true, js: false do
 
     within '#documents' do
       expect(page).to     have_content("211")
+
+      expect(page).not_to have_content("111")
+      expect(page).not_to have_content("311")
+      expect(page).not_to have_content('411')
+    end
+  end
+  it 'can get years with less than 4 digits' do
+    visit root_path
+    click_on 'search'
+
+    within '#documents' do
+      expect(page).to have_content("111")
+      expect(page).to have_content("211")
+      expect(page).to have_content("311")
+      expect(page).to have_content("411")
+    end
+
+    fill_in 'range_year_isim_begin', with: '610'
+    fill_in 'range_year_isim_end', with: '950'
+    click_on 'Apply'
+
+    within '#documents' do
+      expect(page).to have_content("111")
+
+      expect(page).not_to have_content("211")
       expect(page).not_to have_content("311")
       expect(page).not_to have_content('411')
     end
