6525093b42d6ae7f6b94fe0f218516b246da69bd
martinlovell <32851993+martinlovell@users.noreply.github.com>
2020-12-15 18:57:25 -0500

    Update yale only check (#447)
    * update check for displaying metadata and forward_ip to proxy
    
    * update tests so logged out user does not have allowed ip
    
    * Update placeholder image conditional

--- spec/system/access_restrictions_spec.rb
+++ spec/system/access_restrictions_spec.rb
@@ -24,6 +24,17 @@ RSpec.describe "access restrictions", type: :system, clean: true do
   end
 
   context "an unauthenticated user" do
+    around do |example|
+      original_yale_networks = ENV['YALE_NETWORK_IPS']
+      ENV['YALE_NETWORK_IPS'] = "101.10.5.4,3.4.2.3"
+      example.run
+      ENV['YALE_NETWORK_IPS'] = original_yale_networks
+    end
+
+    before do
+      allow_any_instance_of(ActionDispatch::Request).to receive(:remote_ip).and_return('109.10.5.4')
+    end
+
     it 'displays public and yale-only but NOT private works in search results' do
       visit '/catalog?search_field=all_fields&q='
 
@@ -62,7 +73,37 @@ RSpec.describe "access restrictions", type: :system, clean: true do
     end
 
     it "displays universal viewer for yale-only works" do
+      visit solr_document_path(yale_work[:id])
+      expect(page.html).to match(/universal-viewer-iframe/)
+    end
+  end
+
+  context "an user on the network" do
+    around do |example|
+      original_yale_networks = ENV['YALE_NETWORK_IPS']
+      ENV['YALE_NETWORK_IPS'] = "101.10.5.4,3.4.2.3"
+      example.run
+      ENV['YALE_NETWORK_IPS'] = original_yale_networks
+    end
+
+    before do
+      allow_any_instance_of(ActionDispatch::Request).to receive(:remote_ip).and_return('101.10.5.4')
+    end
+
+    it 'displays public and yale-only but NOT private works in search results' do
+      visit '/catalog?search_field=all_fields&q='
+      expect(page).to have_content('A General dictionary of the English language')
+      expect(page).to have_content('[Map of China]. [yale-only copy]')
+      expect(page).not_to have_content('[Map of China]. [private copy]')
+    end
+
+    it "displays universal viewer for public works" do
       visit solr_document_path(public_work[:id])
+      expect(page.html).to match(/universal-viewer-iframe/)
+    end
+
+    it "displays universal viewer for yale-only works" do
+      visit solr_document_path(yale_work[:id])
       expect(page.html).to match(/universal-viewer-iframe/)
     end
   end
