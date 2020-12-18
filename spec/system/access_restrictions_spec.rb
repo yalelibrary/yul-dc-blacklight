a27eabff68939b8a529397c187427665e220b8f5
Martin Lovell <martin.lovell@yale.edu>
2020-12-18 15:39:22 -0500

    add more private item tests

--- spec/system/access_restrictions_spec.rb
+++ spec/system/access_restrictions_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe "access restrictions", type: :system, clean: true do
       expect(page.html).to have_content("[Map of China]. [yale-only copy]")
     end
 
-    it "does NOT display universal viewer for private works" do
+    it "does NOT display universal viewer or metadata for private works" do
       visit solr_document_path(private_work[:id])
       expect(page.html).to have_content("You are not authorized to view this item.")
       expect(page.html).not_to match(/universal-viewer-iframe/)
@@ -84,6 +84,12 @@ RSpec.describe "access restrictions", type: :system, clean: true do
     it "displays universal viewer for yale-only works" do
       visit solr_document_path(yale_work[:id])
       expect(page.html).to match(/universal-viewer-iframe/)
+    end
+
+    it "does not displays universal viewer or metadata for private works" do
+      visit solr_document_path(private_work[:id])
+      expect(page.html).not_to match(/universal-viewer-iframe/)
+      expect(page.html).not_to have_content("[Map of China]. [private copy]")
     end
   end
 
@@ -117,7 +123,7 @@ RSpec.describe "access restrictions", type: :system, clean: true do
       expect(page.html).to have_content("[Map of China]. [yale-only copy]")
     end
 
-    it "displays universal viewer for private works" do
+    it "does not displays universal viewer or metadata for private works" do
       visit solr_document_path(private_work[:id])
       expect(page.html).not_to match(/universal-viewer-iframe/)
       expect(page.html).not_to have_content("[Map of China]. [private copy]")
