f06b5053bdfd7c0f89f1c02c1f90114fc88f83d4
Martin Lovell <martin.lovell@yale.edu>
2021-03-22 15:17:53 -0400

    Starting mods document format
    Co-authored-by: Maggie Zhao <maggie.zhao@yale.edu>
    Co-authored-by: Lakeisha Robinson <lakeisha.robinson@yale.edu>
    Co-authored-by: Alisha Evans <alisha@notch8.com>

--- app/models/concerns/mods_solr_document.rb
+++ app/models/concerns/mods_solr_document.rb
@@ -6,6 +6,10 @@ module ModsSolrDocument
   def to_oai_mods
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.mods('xmlns:mods' => 'http://www.loc.gov/mods/v3', 'version' => '3.4') do
+        xml['mods'].identifier({ type: 'ladybird' }, "oid#{self[:id]}")
+        xml['mods'].identifier({ displayLabel: 'Accession Number', type: 'local' }, self[:accessionNumber_ssi]) if self[:accessionNumber_ssi].present?
+        xml['mods'].identifier({ displayLabel: 'Barcode', type: 'local' }, self[:orbisBarcode_ssi]) if self[:orbisBarcode_ssi].present?
+
         xml['mods'].titleInfo do
           self[:title_tesim]&.each { |title| xml['mods'].title title.to_s }
         end
--- spec/requests/catalog_controller_request_spec.rb
+++ spec/requests/catalog_controller_request_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe "/catalog", clean: true, type: :request do
     solr.commit
   end
 
+  def ns_hash
+    { 'mods' => 'http://www.loc.gov/mods/v3' }
+  end
+
   describe 'GET /oai?verb=ListRecords&metadataPrefix=oai_mods' do
     it 'returns success to ListRecords' do
       get "/catalog/oai?verb=ListRecords&metadataPrefix=oai_mods"
@@ -24,6 +28,22 @@ RSpec.describe "/catalog", clean: true, type: :request do
     it 'returns success to ListFormats' do
       get "/catalog/oai?verb=ListMetadataFormats"
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns properly formatted identifier with GetRecord' do
+      get "/catalog/oai?verb=GetRecord&metadataPrefix=oai_mods&identifier=oai:collections.library.yale.edu:#{WORK_WITH_PUBLIC_VISIBILITY[:id]}"
+      expect(response).to have_http_status(:success)
+      xml = Nokogiri::XML(response.body)
+      identifier = xml.xpath('//mods:identifier[@type=\'ladybird\']', ns_hash)
+      expect(identifier.text).to eq("oid#{WORK_WITH_PUBLIC_VISIBILITY[:id]}")
+    end
+
+    it 'returns properly formatted title with GetRecord' do
+      get "/catalog/oai?verb=GetRecord&metadataPrefix=oai_mods&identifier=oai:collections.library.yale.edu:#{WORK_WITH_PUBLIC_VISIBILITY[:id]}"
+      expect(response).to have_http_status(:success)
+      xml = Nokogiri::XML(response.body)
+      title = xml.xpath('//mods:titleInfo', ns_hash).xpath('//mods:title', ns_hash)
+      expect(title.text).to eq(WORK_WITH_PUBLIC_VISIBILITY[:title_tesim].first)
     end
   end
 end
