29447e5428649c2b89a8bf35be71acd0a235800c
Martin Lovell <martin.lovell@yale.edu>
2021-03-25 14:57:49 -0400

    Add xlink namespace

--- app/models/concerns/mods_solr_document.rb
+++ app/models/concerns/mods_solr_document.rb
@@ -7,7 +7,7 @@ module ModsSolrDocument
   # rubocop:disable Metrics/BlockLength,Metrics/MethodLength,Metrics/PerceivedComplexity,Metrics/AbcSize
   def to_oai_mods
     builder = Nokogiri::XML::Builder.new do |xml|
-      xml.mods('xmlns:mods' => 'http://www.loc.gov/mods/v3', 'version' => '3.4') do
+      xml.mods('xmlns:mods' => 'http://www.loc.gov/mods/v3', 'version' => '3.4', 'xmlns:xlink' => 'http://www.w3.org/1999/xlink') do
         xml['mods'].identifier({ type: 'ladybird' }, "oid#{self[:id]}")
         xml['mods'].identifier({ displayLabel: 'Accession Number', type: 'local' }, self[:accessionNumber_ssi]) if self[:accessionNumber_ssi].present?
         xml['mods'].identifier({ displayLabel: 'Barcode', type: 'local' }, self[:orbisBarcode_ssi]) if self[:orbisBarcode_ssi].present?
