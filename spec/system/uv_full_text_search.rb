d31a913c5a08f1655bca77084e9865ebfea2d4fb
mikeapp <michael.appleby@yale.edu>
2021-05-27 19:31:13 -0400

    Retrieves full text search query from user session and passes it to Universal Viewer.  UV is configured to accept the search param in its iframe URL.

---
+++ spec/system/uv_full_text_search.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Searching for full text and navigate to the UV', type: :system, js: true, clean: true do
+  before do
+    stub_request(:get, 'https://yul-dc-development-samples.s3.amazonaws.com/manifests/01/1.json')
+      .to_return(status: 200, body: File.open(File.join('spec', 'fixtures', '2041002.json')).read)
+
+    solr = Blacklight.default_index.connection
+    solr.add([dog])
+    solr.commit
+  end
+
+  let(:dog) { ADVANCED_SEARCH_TESTING_1 }
+
+  it 'adds the fulltext search parameter to the iframe src URL in advanced search' do
+    visit search_catalog_path
+    click_on "Advanced Search"
+    fill_in 'fulltext_tsim_advanced', with: 'fulltext'
+    click_on 'SEARCH'
+    click_on 'Record 1'
+    uv_iframe = page.find('.universal-viewer-iframe')
+    expect(uv_iframe[:src]).to include('q=fulltext')
+  end
+
+  it 'adds the fulltext search parameter to the iframe src URL in simple search' do
+    visit search_catalog_path
+    select('Full Text', from: 'search_field')
+    fill_in 'q', with: 'fulltext'
+    within '.input-group-append' do
+      click_button("Search")
+    end
+    click_on 'Record 1'
+    uv_iframe = page.find('.universal-viewer-iframe')
+    expect(uv_iframe[:src]).to include('q=fulltext')
+  end
+end
