42e985a71ede5b6f1137f020bd6d3bb3447ac4cd
FCRodriguez7 <frederick.rodriguez@yale.edu>
2021-06-09 15:26:06 -0400

    Modified highlighting in simple search only

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -164,7 +164,7 @@ class CatalogController < ApplicationController
       "hl.fragsize": 40
     }
 
-    disp_req_highlight_on_search_params = {
+    disp_req_fieldmatch_on_search_params = {
       'hl.requireFieldMatch': true
     }
     # Have BL send all facet field names to Solr, which has been the default
@@ -174,12 +174,12 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field 'creator_tesim', label: 'Creator', highlight: true, solr_params: disp_req_highlight_on_search_params
-    config.add_index_field 'date_ssim', label: 'Published / Created', highlight: true, solr_params: disp_req_highlight_on_search_params
-    config.add_index_field 'callNumber_tesim', label: 'Call Number', highlight: true, solr_params: disp_req_highlight_on_search_params
-    config.add_index_field 'sourceTitle_tesim', label: 'Collection Title', highlight: true, solr_params: disp_req_highlight_on_search_params
+    config.add_index_field 'creator_tesim', label: 'Creator', highlight: true, solr_params: disp_req_fieldmatch_on_search_params
+    config.add_index_field 'date_ssim', label: 'Published / Created', highlight: true, solr_params: disp_req_fieldmatch_on_search_params
+    config.add_index_field 'callNumber_tesim', label: 'Call Number', highlight: true, solr_params: disp_req_fieldmatch_on_search_params
+    config.add_index_field 'sourceTitle_tesim', label: 'Collection Title', highlight: true, solr_params: disp_req_fieldmatch_on_search_params
     config.add_index_field 'imageCount_isi', label: 'Image Count'
-    config.add_index_field 'resourceType_tesim', label: 'Resource Type', highlight: true, solr_params: disp_req_highlight_on_search_params
+    config.add_index_field 'resourceType_tesim', label: 'Resource Type', highlight: true, solr_params: disp_req_fieldmatch_on_search_params
     config.add_index_field 'fulltext_tesim', label: 'Full Text', highlight: true, solr_params: disp_highlight_on_search_params.merge({ 'hl.snippets': 4 }), helper_method: :fulltext_snippet_separation
     config.add_index_field 'abstract_tesim', label: 'Abstract', highlight: true, solr_params: disp_highlight_on_search_params
     config.add_index_field 'alternativeTitle_tesim', label: 'Alternative Title', highlight: true, solr_params: disp_highlight_on_search_params
@@ -465,7 +465,15 @@ class CatalogController < ApplicationController
       field.include_in_advanced_search = false
       field.solr_parameters = {
         qf: 'fulltext_tesim',
-        pf: ''
+        pf: '',
+        'hl': true,
+        'hl.method': 'original',
+        'hl.requireFieldMatch': true,
+        'hl.usePhraseHighlighter': true,
+        'hl.preserveMulti': false,
+        "hl.simple.pre": "<span class='search-highlight'>",
+        "hl.simple.post": "</span>",
+        "hl.fragsize": 40
       }
     end
 
