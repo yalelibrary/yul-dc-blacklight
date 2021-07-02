704461ba6e49c1ce880f4f4998f38eba02954bf2
Maggie Zhao <LixiaZhao@yale.edu>
2021-07-02 11:46:01 -0400

    delete config index

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -169,7 +169,6 @@ class CatalogController < ApplicationController
     config.add_index_field 'subjectName_tesim', label: 'Subject (Name)', highlight: true, solr_params: disp_highlight_on_search_params
     config.add_index_field 'subjectTopic_tesim', label: 'Subject (Topic)', highlight: true, solr_params: disp_highlight_on_search_params
     config.add_index_field 'sourceCreated_tesim', label: 'Collection Created', highlight: true, solr_params: disp_highlight_on_search_params
-    config.add_index_field 'repository_ssi', label: 'Repository', highlight: true, solr_params: disp_highlight_on_search_params
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
     #
