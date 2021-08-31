de8f9d50c2c2e9591741065e01ad98e42a62acf2
K8Sewell <kait@notch8.com>
2021-08-31 13:36:04 -0700

    Adds records for search

--- spec/system/archival_sort_spec.rb
+++ spec/system/archival_sort_spec.rb
@@ -4,7 +4,11 @@ require 'rails_helper'
 RSpec.describe 'archival sort', type: :system, clean: true do
   before do
     solr = Blacklight.default_index.connection
-    solr.add([dog])
+    solr.add([dog,
+              cat,
+              llama,
+              eagle,
+              puppy])
     solr.commit
     visit '/catalog?per_page=1&q=&search_field=all_fields'
   end
@@ -12,13 +16,14 @@ RSpec.describe 'archival sort', type: :system, clean: true do
   let(:dog) do
     {
       id: '111',
+      title_tesim: ['Amor Perro'],
       creator_tesim: 'Me and You',
       date_ssim: '1999',
       resourceType_ssim: 'Archives or Manuscripts',
-      archivalSort_ssi: '00001.00038',
       callNumber_tesim: 'Beinecke MS 801',
       imageCount_isi: '23',
       visibility_ssi: 'Public',
+      archivalSort_ssi: '00001.00038',
       ancestorTitles_tesim: ['Beinecke Rare Book and Manuscript Library (BRBL)',
                              'Osborn Manuscript Files (OSB MSS FILE)',
                              'Numerical Sequence: 17975-19123',
@@ -33,13 +38,14 @@ RSpec.describe 'archival sort', type: :system, clean: true do
   let(:cat) do
     {
       id: '222',
+      title_tesim: ['Amor Gato'],
       creator_tesim: 'Me and You',
       date_ssim: '1999',
       resourceType_ssim: 'Archives or Manuscripts',
-      archivalSort_ssi: '00000.00040',
       callNumber_tesim: 'Beinecke MS 801',
       imageCount_isi: '23',
       visibility_ssi: 'Public',
+      archivalSort_ssi: '00006.00040',
       ancestorTitles_tesim: ['Beinecke Rare Book and Manuscript Library (BRBL)',
                              'Script Files',
                              'Numerical Sequence: 17975-19123',
@@ -51,9 +57,82 @@ RSpec.describe 'archival sort', type: :system, clean: true do
     }
   end
 
+  let(:llama) do
+    {
+      id: '333',
+      title_tesim: ['Amor Llama'],
+      creator_tesim: 'Anna Elizabeth Dewdney',
+      date_ssim: '1999',
+      repository_ssi: 'Yale University Arts Library',
+      collection_title_ssi: ['AAA'],
+      format: 'text',
+      language_ssim: 'la',
+      visibility_ssi: 'Public',
+      genre_ssim: 'Maps',
+      resourceType_ssim: 'Maps, Atlases & Globes',
+      archivalSort_ssi: '00000.00040',
+      ancestorTitles_tesim: ['Beinecke Rare Book and Manuscript Library (BRBL)',
+                             'Llama Files',
+                             'Numerical Sequence: 17975-19123',
+                             'BURNEY, SARAH HARRIET, 1772-1844',
+                             'Level3',
+                             'Level2',
+                             'Level1',
+                             'Level0']
+    }
+  end
+
+  let(:eagle) do
+    {
+      id: '444',
+      title_tesim: ['Aquila Eccellenza'],
+      creator_tesim: 'Andrew Norriss',
+      date_ssim: '1999',
+      format: 'still image',
+      language_ssim: 'it',
+      visibility_ssi: 'Public',
+      genre_ssim: 'Manuscripts',
+      resourceType_ssim: 'Archives or Manuscripts',
+      archivalSort_ssi: '00020.00040',
+      ancestorTitles_tesim: ['Beinecke Rare Book and Manuscript Library (BRBL)',
+                             'Eagle Files',
+                             'Numerical Sequence: 17975-19123',
+                             'BURNEY, SARAH HARRIET, 1772-1844',
+                             'Level3',
+                             'Level2',
+                             'Level1',
+                             'Level0']
+    }
+  end
+
+  let(:puppy) do
+    {
+      id: '555',
+      title_tesim: ['Rhett Lecheire'],
+      creator_tesim: 'Paulo Coelho',
+      date_ssim: '1999',
+      format: 'text',
+      language_ssim: 'fr',
+      visibility_ssi: 'Public',
+      genre_ssim: 'Animation',
+      resourceType_ssim: 'Archives or Manuscripts',
+      archivalSort_ssi: '04000.00040',
+      ancestorTitles_tesim: ['Beinecke Rare Book and Manuscript Library (BRBL)',
+                             'Puppy Files',
+                             'Numerical Sequence: 17975-19123',
+                             'BURNEY, SARAH HARRIET, 1772-1844',
+                             'Level3',
+                             'Level2',
+                             'Level1',
+                             'Level0']
+    }
+  end
+
+
   context 'search results sorts based on archivalSort conditions' do
     it 'displays the first thing first' do
       expect(page).to have_content('Osborn Manuscript Files (OSB MSS FILE)')
+      expect(page).not_to have_content('Script Files')
     end
   end
 end
