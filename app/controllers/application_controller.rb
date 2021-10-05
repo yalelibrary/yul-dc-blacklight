2857bff926b01e74c42211dd3daffabd97461739
K8Sewell <kait@notch8.com>
2021-10-05 13:29:40 -0700

    Change record not found display

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,10 @@ class ApplicationController < ActionController::Base
 
   layout :determine_layout if respond_to? :layout
 
-  rescue_from Blacklight::Exceptions::RecordNotFound do
-    render template: 'errors/not_found'
+  rescue_from Blacklight::Exceptions::RecordNotFound, with: :not_found
+
+  def not_found
+    send_file 'public/not_found.html', disposition: 'inline', type: 'text/html; charset=utf-8', status: 404
   end
 
   def landing
