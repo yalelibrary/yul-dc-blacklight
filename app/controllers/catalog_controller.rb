0d639ba40108956f9d455245b3a9b2cfd3e20a1b
Amanda <amanda@scientist.com>
2022-08-31 16:32:40 -0700

    Adds digitization funding source to catalog item show page

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -227,6 +227,7 @@ class CatalogController < ApplicationController
     config.add_show_field 'extent_ssim', label: 'Extent', metadata: 'description', helper_method: :join_with_br
     config.add_show_field 'extentOfDigitization_ssim', label: 'Extent of Digitization', metadata: 'description'
     config.add_show_field 'digitization_note_tesi', label: 'Digitization Note', metadata: 'description'
+    config.add_show_field 'digitization_funding_source_tesi', label: 'Digitization Funding Source', metadata: 'description'
     config.add_show_field 'projection_tesim', label: 'Projection', metadata: 'description'
     config.add_show_field 'scale_tesim', label: 'Scale', metadata: 'description'
     config.add_show_field 'coordinateDisplay_ssim', label: 'Coordinates', metadata: 'description'
