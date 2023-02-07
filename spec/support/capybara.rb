f3d13fd967b72b510117b9f6b896656e873ddd29
K8Sewell <kait@notch8.com>
2023-02-07 10:12:53 -0600

    Replace selenium capabilities with options

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,16 +7,13 @@ Capybara.default_driver = :rack_test
 # Capybara.server = :puma, { Silent: false }
 ENV['WEB_HOST'] ||= `hostname -s`.strip
 
-capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-  chromeOptions: {
-    args: %w[disable-gpu no-sandbox whitelisted-ips window-size=1400,1400]
-  }
-)
+options = Selenium::WebDriver::Options.chrome
+options.page_load_strategy = :none
 
 Capybara.register_driver :chrome do |app|
   d = Capybara::Selenium::Driver.new(app,
                                      browser: :remote,
-                                     desired_capabilities: capabilities,
+                                     options: options,
                                      url: "http://chrome:4444/wd/hub")
   # Fix for capybara vs remote files. Selenium handles this for us
   d.browser.file_detector = lambda do |args|
