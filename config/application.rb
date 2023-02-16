26d462cecc6d89e39a5df0f9c7ab8cc755bfc084
K8Sewell <kait@notch8.com>
2023-02-16 10:06:41 -0600

    Fix trailing whitespace

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module BlacklightYul
     STDOUT.sync = true # turn off log buffering
     config.rails_semantic_logger.add_file_appender = false # turn off regular file appenders
     config.semantic_logger.add_appender(io: STDOUT, level: config.log_level, formatter: config.rails_semantic_logger.format)
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
