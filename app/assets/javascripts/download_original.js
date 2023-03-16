8ee74a619731aa36a6e486bafdfa6614138ffb8b
K8Sewell <kait@notch8.com>
2023-03-16 12:53:07 -0500

    Address bugs

--- app/assets/javascripts/download_original.js
+++ app/assets/javascripts/download_original.js
@@ -7,21 +7,25 @@ $(document).on('turbolinks:load', function () {
         const childOid = fullPath.replace(/\D/g, '');
 
         // check if file is on S3
-        const available = $.ajax(hostPath + '/download/tiff/' + childOid + '/available');
+        function available() { 
+            $.ajax(hostPath + '/download/tiff/' + childOid + '/available');
+        };
 
         // create loop that checks for the file every minute
         function sleep(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
-        }
+        };
         do {
             sleep(60000);
-            available;
+            available();
         } while (available.responseText == 'false')
 
         if (available.responseText == 'true') {
             // trigger download once
-            const download = $.ajax(hostPath + '/download/tiff/' + childOid)
-            download
+            function download() { 
+                $.ajax(hostPath + '/download/tiff/' + childOid)
+            };
+            download();
         }
     }
 
