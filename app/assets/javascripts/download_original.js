ef4df6e401349307a876e40658e4fd3f1816cd8b
K8Sewell <kait@notch8.com>
2023-03-16 13:35:03 -0500

    Refactor js

--- app/assets/javascripts/download_original.js
+++ app/assets/javascripts/download_original.js
@@ -1,32 +1,23 @@
 $(document).on('turbolinks:load', function () {
     const stagedBlurb = $('.tiff-staged');
-
+    const fullPath = window.location.pathname;
+    const hostPath = window.location.origin;
+    const childOid = fullPath.replace(/\D/g, '');
     if (stagedBlurb.length) {
-        const fullPath = window.location.pathname;
-        const hostPath = window.location.origin;
-        const childOid = fullPath.replace(/\D/g, '');
-
-        // check if file is on S3
-        function available() { 
-            $.ajax(hostPath + '/download/tiff/' + childOid + '/available');
-        };
-
-        // create loop that checks for the file every minute
-        function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        };
-        do {
-            sleep(60000);
-            available();
-        } while (available.responseText == 'false')
-
-        if (available.responseText == 'true') {
-            // trigger download once
-            function download() { 
-                $.ajax(hostPath + '/download/tiff/' + childOid)
-            };
-            download();
-        }
+        check_availability(childOid, hostPath);
     }
-
 })
+
+
+function check_availability(childOid, hostPath) {
+    $.ajax({
+        url: hostPath + '/download/tiff/' + childOid + '/available',
+        complete: function(r) {
+            if (r.responseText === 'false') {
+                setTimeout(()=> check_availability(childOid, hostPath), 30000)
+            } else if (r.responseText === 'true') {
+                window.location.href = hostPath + '/download/tiff/' + childOid
+            }
+        }
+    });
+}
