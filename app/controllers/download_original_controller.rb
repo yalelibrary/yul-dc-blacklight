b475a88fca64b5800de4c8667a4f119ab3e15019
mikeapp <michael.appleby@me.com>
2023-03-20 10:38:53 -0400

    Set content-disposition to encourage browser to download image  (#885)
    * Set content-disposition to encourage browser to download image rather than display it.
    
    * Close window after download

--- app/controllers/download_original_controller.rb
+++ app/controllers/download_original_controller.rb
@@ -12,8 +12,8 @@ class DownloadOriginalController < ApplicationController
     if S3Service.exists_in_s3(tiff_pairtree_path)
       send_tiff
     else
-      stage_download(params)
-      redirect_to "#{root_url}download/tiff/#{params[:child_oid]}/staged", status: 303
+      stage_download
+      redirect_to "#{root_url}download/tiff/#{child_oid}/staged", status: 303
     end
   end
 
@@ -35,32 +35,32 @@ class DownloadOriginalController < ApplicationController
     response.set_header('Content-Type', 'image/tiff')
     response.set_header('X-Robots-Tag', 'noindex')
     response.set_header('Cache-Control', 'no-store')
+    response.set_header('Content-Disposition', "attachment; filename=\"#{child_oid}.tif\"")
     client = Aws::S3::Client.new
     client.get_object(bucket: ENV['S3_DOWNLOAD_BUCKET_NAME'], key: tiff_pairtree_path) do |chunk|
       response.stream.write(chunk)
     end
   rescue StandardError => e
-    Rails.logger.error("TIFF with id [#{params[:child_oid]}] - error: #{e.message}")
+    Rails.logger.error("TIFF with id [#{child_oid}] - error: #{e.message}")
     redirect_to root_path, notice: 'There was an error downloading the file.  Please try again later.'
   ensure
     response.stream.close
   end
 
-  def stage_download(params)
-    Rails.logger.info("TIFF with id [#{params[:child_oid]}] not found - staging for download.")
-    url = URI.parse("#{ENV['MANAGEMENT_HOST']}/api/download/stage/child/#{params[:child_oid]}")
+  def stage_download
+    Rails.logger.info("TIFF with id [#{child_oid}] not found - staging for download.")
+    url = URI.parse("#{ENV['MANAGEMENT_HOST']}/api/download/stage/child/#{child_oid}")
     req = Net::HTTP::Get.new(url.path)
     con = Net::HTTP.new(url.host, url.port)
     con.start { |http| http.request(req) }
   end
 
   def tiff_pairtree_path
-    pairtree = Partridge::Pairtree.oid_to_pairtree(params[:child_oid])
-    File.join('download', 'tiff', pairtree, "#{params[:child_oid]}.tif")
+    pairtree = Partridge::Pairtree.oid_to_pairtree(child_oid)
+    File.join('download', 'tiff', pairtree, "#{child_oid}.tif")
   end
 
   def search_for_item
-    child_oid = params[:child_oid]
     search_state[:rows] = 1
     search_service_class.new(config: blacklight_config, search_state: search_state, user_params: search_state.to_h, **search_service_context)
     response, document = search_service.search_results do |builder|
@@ -68,5 +68,9 @@ class DownloadOriginalController < ApplicationController
       builder
     end
     [response, document.first]
+  end
+
+  def child_oid
+    Integer(params[:child_oid])
   end
 end
