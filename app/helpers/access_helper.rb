8c34fa4c774b035c6a7884833b295f0c9ef904fb
JP Engstrom <jpengstrom@vpn17225132108.its.yale.internal>
2023-11-01 15:16:42 -0700

    conditionals for signed in users

--- app/helpers/access_helper.rb
+++ app/helpers/access_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module AccessHelper
   def viewable_metadata_visibilities
-    ["Public", "Yale Community Only"]
+    ["Public", "Yale Community Only", "Open with Permission"]
   end
 
   def client_can_view_digital?(document)
@@ -14,6 +14,46 @@ module AccessHelper
     end
     false
   end
+
+  def client_can_view_owp?(document)
+    # api response method
+    Rails.logger.warn("starting client can view digital check for #{request.env['HTTP_X_ORIGIN_URI']}")
+    case document['visibility_ssi']
+    when 'Open with Permission' && user_has_permission?(document)
+      return true
+    end
+    false
+  end
+
+  def is_object_owp?(document)
+    case document['visibility_ssi']
+    when 'Open with Permission'
+      return true
+    end
+    false
+  end
+
+  def user_has_permission?(document)
+    parent_oid = document[:parent_ssi]
+    if current_user
+      retrieve_user_permissions['permissions'].each do |permission|
+        if (permission['oid'] == parent_oid) && permission['access_until'] > Date.zone.today
+          true
+        end
+      end
+    end
+    false
+  end
+
+  def retrieve_user_permissions
+    url = URI.parse("http://localhost:3001/management/api/permission_sets/#{current_user.sub}") if current_user
+    req = Net::HTTP::Get.new(url)
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    res = https.request(req)
+    JSON.parse(res.body)
+  end
+
 
   def client_can_view_metadata?(document)
     viewable_metadata_visibilities.include? document['visibility_ssi']
@@ -33,5 +73,19 @@ module AccessHelper
       return "Please log in using your Yale NetID or contact library staff to inquire about access to a physical copy."
     end
     "You are not authorized to view this item."
+  end
+
+  def owp_login_message(document)
+    case document['visibility_ssi']
+    when 'Open with Permission'
+      return "The material in this folder is open for research use only with permission. Researchers who wish to gain access or who have received permission to view this item, please #{link_to 'log in', user_openid_connect_omniauth_authorize_path, method: :post} to your account to request permission or to view the materials in this folder.".html_safe
+    end
+  end
+
+  def owp_restriction_message(document)
+    case document['visibility_ssi']
+    when 'Open with Permission'
+      return "You are currently logged in to your account. However, you do not have permission to view this folder. If you would like to request permission, please fill out this form."
+    end
   end
 end
