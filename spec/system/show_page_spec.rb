97f66712b7266cea5023d89f3c4d4d8f9a8eb1cf
JP Engstrom <jpengstrom@vpn1722517119.its.yale.internal>
2023-11-14 11:01:48 -0800

    update specs for tools partial

--- spec/system/show_page_spec.rb
+++ spec/system/show_page_spec.rb
@@ -337,9 +337,6 @@ RSpec.describe 'Show Page', type: :system, js: true, clean: true do
     it 'displays login message when accessing an OwP object without access' do
       visit 'catalog/54321'
       expect(page).to have_content "You are currently logged in to your account. However, you do not have permission to view this folder. If you would like to request permission, please fill out this form."
-      expect(page).not_to have_content "TOOLS"
-      expect(page).not_to have_content "Manifest Link"
-      expect(page).not_to have_content "Download as PDF"
     end
   end
   # rubocop:enable Layout/LineLength
