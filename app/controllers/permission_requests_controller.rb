21fbd5695b2374128d73a64b849f598447a4d85d
JP Engstrom <jpengstrom@macbook-pro.lan>
2024-01-19 10:39:32 -0800

    rubocop

--- app/controllers/permission_requests_controller.rb
+++ app/controllers/permission_requests_controller.rb
@@ -2,7 +2,7 @@
 
 class PermissionRequestsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def prep_request
     if current_user.nil?
       redirect_to("#{ENV['BLACKLIGHT_HOST']}/catalog/#{params[:oid]}", notice: 'Please log in to request access to these materials.')
