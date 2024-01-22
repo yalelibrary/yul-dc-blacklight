d37c799e4def6ace48640faa7e3bd08d37320ac0
K8Sewell <kait@notch8.com>
2024-01-22 14:31:44 -0600

    Remove old override

--- app/components/yul/system/modal_component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Yul
-  module System
-    class ModalComponent < ViewComponent::Base
-      with_content_areas :prefix, :header, :title, :body, :footer
-    end
-  end
-end
--- spec/system/modal_component_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-require 'rails_helper'
-
-RSpec.describe Yul::System::ModalComponent, type: :system do
-  it 'has expected content' do
-    expect(described_class.with_content_areas).to eq []
-  end
-end
