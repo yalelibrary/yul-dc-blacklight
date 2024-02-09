02ff7cc3d84796fcd49d7f6fa4416ca66d67c6ef
JP Engstrom <jpengstrom@macbook-pro.lan>
2024-02-09 09:33:57 -0800

    fallback for nil terms

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -642,7 +642,9 @@ class CatalogController < ApplicationController
     @response, @document = search_service.fetch(params[:oid])
     if current_user && @document['visibility_ssi'] == 'Open with Permission'
       @permission_set_terms = retrieve_permission_set_terms
-      if user_owp_permissions['permission_set_terms_agreed']&.include?(@permission_set_terms['id'])
+      if @permission_set_terms.nil?
+        redirect_back(fallback_location: "#{ENV['BLACKLIGHT_HOST']}/catalog/#{params[:oid]}", notice: "We are unable to complete your access request at this time. For more information about this object, click the ‘Feedback’ link located at the bottom of this page and fill out the form. We will get back to you as soon as possible.")
+      elsif user_owp_permissions['permission_set_terms_agreed']&.include?(@permission_set_terms['id'])
         render 'catalog/request_form'
       else
         render 'catalog/terms_and_conditions'
