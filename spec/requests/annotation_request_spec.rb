e9f20720690052f61f5f6ad6573e0fd5433d1f85
JP Engstrom <jp@scientist.com>
2024-04-04 10:49:15 -0700

    adds spec coverage for OwP fulltext annotation (#995)
    * adds spec coverage for OwP fulltext annotation
    
    * adds OwP parent
    
    * comma typo
    
    ---------
    
    Co-authored-by: JP Engstrom <jpengstrom@macbook-pro.lan>

--- spec/requests/annotation_request_spec.rb
+++ spec/requests/annotation_request_spec.rb
@@ -26,10 +26,24 @@ RSpec.describe 'AnnotationsController', type: :request, clean: true, js: true do
       "visibility_ssi": "Yale Community Only"
     }
   end
+  let(:owp_work) do
+    {
+      "id": "16189090",
+      "title_tesim": ["[Map of China]. [yale-only copy]"],
+      "visibility_ssi": "Open with Permission"
+    }
+  end
   let(:child_work_yale_only) do
     {
       "id": "998834",
       "parent_ssi": "1618909",
+      "child_fulltext_wstsim": ["This is the full text Yale only"]
+    }
+  end
+  let(:child_work_owp) do
+    {
+      "id": "9988344",
+      "parent_ssi": "16189090",
       "child_fulltext_wstsim": ["This is the full text Yale only"]
     }
   end
@@ -51,7 +65,7 @@ RSpec.describe 'AnnotationsController', type: :request, clean: true, js: true do
   describe 'Full Text' do
     before do
       solr = Blacklight.default_index.connection
-      solr.add([public_work, yale_work, unknown_visibility_work, child_work, child_work_yale_only, child_work_unknown])
+      solr.add([public_work, yale_work, owp_work, unknown_visibility_work, child_work, child_work_yale_only, child_work_unknown, child_work_owp])
       solr.commit
       allow(User).to receive(:on_campus?).and_return(false)
     end
@@ -64,6 +78,10 @@ RSpec.describe 'AnnotationsController', type: :request, clean: true, js: true do
       end
       it 'returns 401 for a full text annotation on Yale Only parent' do
         get '/annotation/oid/1618909/canvas/998834/fulltext'
+        expect(response).to have_http_status(:unauthorized)
+      end
+      it 'returns 401 for a full text annotation on Open with Permission parent' do
+        get '/annotation/oid/1618909/canvas/9988344/fulltext'
         expect(response).to have_http_status(:unauthorized)
       end
       it 'returns 401 for a full text annotation because of mismatch parent' do
@@ -90,6 +108,10 @@ RSpec.describe 'AnnotationsController', type: :request, clean: true, js: true do
         expect(response).to have_http_status(:success)
         expect(response.body).to include("This is the full text Yale only")
       end
+      it 'returns 401 for a full text annotation on Open with Permission parent' do
+        get '/annotation/oid/1618909/canvas/9988344/fulltext'
+        expect(response).to have_http_status(:unauthorized)
+      end
       it 'returns 401 for a full text annotation because of mismatch parent' do
         get '/annotation/oid/2034600/canvas/998834/fulltext'
         expect(response).to have_http_status(:unauthorized)
@@ -113,6 +135,10 @@ RSpec.describe 'AnnotationsController', type: :request, clean: true, js: true do
         get '/annotation/oid/1618909/canvas/998834/fulltext'
         expect(response).to have_http_status(:success)
         expect(response.body).to include("This is the full text Yale only")
+      end
+      it 'returns 401 for a full text annotation on Open with Permission parent' do
+        get '/annotation/oid/1618909/canvas/9988344/fulltext'
+        expect(response).to have_http_status(:unauthorized)
       end
       it 'returns 401 for a full text annotation because of mismatch parent' do
         get '/annotation/oid/2034600/canvas/998834/fulltext'
