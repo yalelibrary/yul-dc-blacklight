89cb31adb1cd322dba355bfd28a8880b000b569a
JP Engstrom <jpengstrom@macbook-pro.lan>
2024-06-11 15:32:16 -0700

    adds current_user checks for bookmarks

---
+++ app/components/blacklight/document/bookmark_component.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Blacklight
+  module Document
+    # Render a bookmark widget to bookmark / unbookmark a document
+    class BookmarkComponent < Blacklight::Document::ActionComponent
+      # @param [Blacklight::Document] document
+      # @param [Blacklight::Configuration::ToolConfig] action
+      # @param [Boolean] checked
+      # @param [Object] bookmark_path the rails route to use for bookmarks
+      def initialize(document:, action: nil, checked: nil, bookmark_path: nil, **kwargs)
+        @document = document
+        @checked = checked
+        @bookmark_path = bookmark_path
+        super(document: document, action: action, **kwargs)
+      end
+
+      def bookmarked?
+        return @checked unless @checked.nil?
+
+        helpers.bookmarked? @document
+      end
+
+      def bookmark_path
+        @bookmark_path || helpers.bookmark_path(@document)
+      end
+    end
+  end
+end
