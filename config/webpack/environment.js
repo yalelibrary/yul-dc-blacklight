1087ae6cdfc1414876a71ddc3b29a384ce224181
K8Sewell <kait@notch8.com>
2024-07-19 16:16:50 -0500

    Upgrade gems and packages

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,39 @@
+// const { environment } = require('@rails/webpacker')
+
+// module.exports = environment
+
+
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+
+$: 'jquery',
+
+jQuery: 'jquery',
+
+Popper: ['popper.js', 'default']
+
+}))
+
+// needed when we upgraded to webpack v5
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  }
+};
+
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
+environment.config.merge(customConfig);
 
 module.exports = environment
