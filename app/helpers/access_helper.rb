1e8f92f1dd510868ea5f2c4d06b3c67d3bd93513
K8Sewell <kait@notch8.com>
2024-09-05 00:52:51 -0500

    Switch from netid to uid

--- app/helpers/access_helper.rb
+++ app/helpers/access_helper.rb
@@ -101,7 +101,7 @@ module AccessHelper
     return nil if current_user.nil?
     # #{ENV['MANAGEMENT_HOST']}
     # for local debugging - http://yul-dc-management-1:3001/management or http://yul-dc_management_1:3001/management
-    url = URI.parse("#{ENV['MANAGEMENT_HOST']}/api/permission_sets/#{document}/#{current_user.netid}")
+    url = URI.parse("#{ENV['MANAGEMENT_HOST']}/api/permission_sets/#{document}/#{current_user.uid}")
     response = Net::HTTP.get_response(url, { 'Authorization' => "Bearer #{ENV['OWP_AUTH_TOKEN']}" })
     JSON.parse(response.body)
   end
