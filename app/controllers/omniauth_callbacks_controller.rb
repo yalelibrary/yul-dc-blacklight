96c3779720ae21ad66b76db72013d4ded4fbee30
Michael Appleby <michael.appleby@yale.edu>
2024-09-12 08:55:32 -0400

    Rubocop

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -24,14 +24,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
             email: auth.info.email
           )
       end
-      # set flag in session for AI-authorized users
-      groups = auth.extra.raw_info['cognito:groups']
-      if groups
-        ai_group = groups.find { |g| 'ai-user' == g }
-        if ai_group
-          session[:show_ai_option] = true
-        end
-      end
+      set_ai_session
     else
       # Login for non-yale users without a net_id
       @user = User.where(provider: auth.provider, uid: auth.uid, sub: sub).first
@@ -56,6 +49,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # rubocop:enable Metrics/PerceivedComplexity
 
   protected
+
+  def set_ai_session
+    # set flag in session for AI-authorized users
+    groups = auth.extra.raw_info['cognito:groups']
+    ai_group = groups.find { |g| 'ai-user' == g } if groups
+    session[:show_ai_option] = true if ai_group
+  end
 
   def after_omniauth_failure_path_for(_resource)
     root_path
