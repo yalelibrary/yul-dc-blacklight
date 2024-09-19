7dbae6fddb1cb9adf2b3590692d86afceb10b94c
JP Engstrom <jp@scientist.com>
2024-09-19 10:35:05 -0700

    expands check to response.body (#1060)
    Co-authored-by: JP Engstrom <jpengstrom@macbook-pro.lan>

--- app/helpers/access_helper.rb
+++ app/helpers/access_helper.rb
@@ -87,7 +87,7 @@ module AccessHelper
     # for local debugging - http://yul-dc-management-1:3001/management or http://yul-dc_management_1:3001/management
     url = URI.parse("#{ENV['MANAGEMENT_HOST']}/api/permission_sets/#{@document[:id]}/terms")
     response = Net::HTTP.get_response(url, { 'Authorization' => "Bearer #{ENV['OWP_AUTH_TOKEN']}" })
-    JSON.parse(response.body) unless response.nil?
+    JSON.parse(response.body) unless response.body.nil?
   end
 
   def retrieve_admin_credentials(document)
