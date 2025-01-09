20ab8b25b40dca3f75c6bb8425b52e84e48ef102
K8Sewell <kait@notch8.com>
2025-01-09 11:44:19 -0600

    Add additional connect-src and remove unsafe inline

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,20 +4,20 @@
 # Define an application-wide content security policy.
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
-if ENV["RAILS_ENV"] == 'production' || ENV["RAILS_ENV"] == 'staging'
+# if ENV["RAILS_ENV"] == 'production' || ENV["RAILS_ENV"] == 'staging'
   Rails.application.configure do
     config.content_security_policy do |policy|
       policy.default_src :self, :https
       policy.font_src    :self, 'static.library.yale.edu'
       policy.img_src     :self, :https, :data, "#{ENV['IIIF_IMAGE_BASE_URL']}/"
       policy.object_src  :none
-      policy.script_src  :self, :unsafe_inline, 'siteimproveanalytics.com www.googletagmanager.com'
-      policy.script_src_attr  :self, :unsafe_inline, 'www.googletagmanager.com'
-      policy.script_src_elem  :self, :unsafe_inline, 'siteimproveanalytics.com www.googletagmanager.com'
-      policy.style_src :self, :unsafe_inline
-      policy.style_src_attr :self, :unsafe_inline
-      policy.style_src_elem :self, :unsafe_inline, "#{ENV['IIIF_IMAGE_BASE_URL']}/"
-      policy.connect_src :self, "banner.library.yale.edu www.google-analytics.com #{ENV['IIIF_IMAGE_BASE_URL']}/"
+      policy.script_src  :self, 'siteimproveanalytics.com www.googletagmanager.com'
+      policy.script_src_attr  :self, 'www.googletagmanager.com'
+      policy.script_src_elem  :self, 'siteimproveanalytics.com www.googletagmanager.com'
+      policy.style_src :self
+      policy.style_src_attr :self
+      policy.style_src_elem :self, "#{ENV['IIIF_IMAGE_BASE_URL']}/"
+      policy.connect_src :self, "banner.library.yale.edu www.google-analytics.com region1.google-analytics.com #{ENV['IIIF_IMAGE_BASE_URL']}/"
 
       # Specify URI for violation reports
       unless ENV['CLUSTER_NAME'] == 'local'
@@ -34,4 +34,4 @@ if ENV["RAILS_ENV"] == 'production' || ENV["RAILS_ENV"] == 'staging'
     # Report violations without enforcing the policy.
     # config.content_security_policy_report_only = true
   end
-end
+# end
