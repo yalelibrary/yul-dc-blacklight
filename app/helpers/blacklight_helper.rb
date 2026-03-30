1d1060561866b931cfcfa1623bbfef4e8a549603
JP Engstrom <jpengstrom@MWPF56T1JH.yu.yale.edu>
2026-03-30 10:42:15 -0700

    keep original thumbnails for non access objects

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -618,7 +618,7 @@ module BlacklightHelper
   def render_thumbnail(document, _options)
     img = build_thumbnail_image(document)
     count = document['imageCount_isi'].to_i
-    if count > 1
+    if count > 1 && client_can_view_digital?(document)
       badge = content_tag(:span, "#{count} pages", class: 'multi-image-badge')
       content_tag(:div, img + badge, class: 'multi-image')
     else
