680b2e790be5ddc66f1a7fe66347bcf35e569a97
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2026-04-09 11:28:17 -0500

    Uses Random UUID for Guest Users (#1220)
    * Uses UUID instead of random sequence
    
    * Prevent generation for non html requests
    
    * Add devise helper to test
    
    * Save the key
    
    * Add guest user factory
    
    * Add override for devise
    
    * Makes uuid random

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,5 +8,9 @@ FactoryBot.define do
     uid { FFaker::Internet.user_name }
     sub { "123" }
     provider { "openid" }
+    factory :guest_user do
+      uid { "guest_" + SecureRandom.uuid }
+      sub { FFaker::Number.number(digits: 10).to_s }
+    end
   end
 end
