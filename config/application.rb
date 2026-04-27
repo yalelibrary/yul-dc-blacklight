e4683bc76d8e9f78552abb85a310469d0264b6f8
K8Sewell <kait@notch8.com>
2026-04-27 11:08:57 -0500

    Enable remaining configurations

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module BlacklightYul
     # TODO: remove the line below when load_defaults 7.0 is enacted
     config.active_support.cache_format_version = 7.0
 
-    config.active_storage.variant_processor = :vips
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
