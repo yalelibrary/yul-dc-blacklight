1a1d308158e6741ecda229ddd875949ebbdad2fd
JP Engstrom <jpengstrom@MacBook-Pro.local>
2026-06-09 09:13:06 -0600

    comments

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -6,9 +6,6 @@
 # notations and behaviors.
 Rails.application.config.filter_parameters += [
   :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn,
-  # Identity / PII fields emitted during login and permission requests (see issue #3327).
-  # :sub and :uid are anchored so they don't also scrub the catalog's `subject`
-  # facet params or identifiers like `druid`/`guid` (filter_parameters matches substrings).
   :email, :user_email, :netid, :user_netid, :user_sub,
   :user_full_name, :user_note, :idtoken, :id_token, :authorization,
   /\Asub\z/i, /\Auid\z/i
