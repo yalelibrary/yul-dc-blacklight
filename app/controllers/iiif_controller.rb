eb1505c4bf0a71cd6640a8c073d7df73f6790c1a
Kait Sewell <36549923+K8Sewell@users.noreply.github.com>
2026-06-12 17:09:54 -0500

    Parse Child Oid (#1282)
    * Adds guards for child oid parsing
    
    * Add test for success response

--- app/controllers/iiif_controller.rb
+++ app/controllers/iiif_controller.rb
@@ -6,6 +6,7 @@ class IiifController < ApplicationController
   include CheckAuthorization
 
   before_action :check_authorization
+  rescue_from ActionController::BadRequest, with: :render_bad_request
 
   def show
     render plain: 'success', status: :ok
@@ -15,8 +16,9 @@ class IiifController < ApplicationController
 
   # IIIF doesn't just return the oid, find the child, then find the oid from there
   def search_for_item
-    Rails.logger.warn("starting search for item for #{request.headers['X-Origin-URI']}")
-    child_oid = request.headers['X-Origin-URI'].gsub(/^\/iiif\/2\/(\d+)\/.*/, '\1')
+    origin_uri = request.headers['X-Origin-URI']
+    Rails.logger.warn("starting search for item for #{sanitize_header_value_for_logs(origin_uri)}")
+    child_oid = parse_child_oid_from_origin_uri!(origin_uri)
     # search_state[:q] = { child_oids_ssim: child_oid }
     search_state[:rows] = 1
     search_service_class.new(config: blacklight_config, search_state: search_state, user_params: search_state.to_h, **search_service_context)
@@ -26,5 +28,19 @@ class IiifController < ApplicationController
       builder
     end
     [r, d.first]
+  end
+
+  def parse_child_oid_from_origin_uri!(origin_uri)
+    raise ActionController::BadRequest, 'Missing X-Origin-URI header' if origin_uri.blank?
+
+    match = origin_uri.match(%r{\A/iiif/2/(\d+)(?:/.*)?\z})
+    raise ActionController::BadRequest, 'Malformed X-Origin-URI header' if match.nil?
+
+    match[1]
+  end
+
+  def render_bad_request(exception)
+    Rails.logger.warn("Bad request in IiifController: #{exception.message}")
+    render json: { error: 'bad-request' }, status: :bad_request
   end
 end
--- spec/requests/iiif_request_spec.rb
+++ spec/requests/iiif_request_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe "Iiifs", type: :request do
+RSpec.describe "Iiifs", type: :request, clean: true do
   let(:thumbnail_size) { "!1200,630" }
 
   let(:yale_user) { FactoryBot.create(:user, netid: "net_id") }
@@ -67,6 +67,30 @@ RSpec.describe "Iiifs", type: :request do
 
       expect(response).to have_http_status(:not_found)
     end
+
+    it 'returns bad request when X-Origin-URI header is missing' do
+      get "/check-iiif"
+
+      expect(response).to have_http_status(:bad_request)
+      expect(JSON.parse(response.body)).to eq('error' => 'bad-request')
+    end
+
+    it 'returns bad request when X-Origin-URI header is malformed' do
+      get "/check-iiif", headers: { 'X-Origin-URI' => 'foo' }
+
+      expect(response).to have_http_status(:bad_request)
+      expect(JSON.parse(response.body)).to eq('error' => 'bad-request')
+    end
+
+    it 'sanitizes X-Origin-URI header values in logs' do
+      allow(Rails.logger).to receive(:warn)
+
+      get "/check-iiif", headers: { 'X-Origin-URI' => "/iiif/2/5555555/full/#{thumbnail_size}/0/default.jpg\r\nFORGED_LOG_ENTRY" }
+
+      expect(response).to have_http_status(:bad_request)
+      expect(Rails.logger).to have_received(:warn)
+        .with("starting search for item for /iiif/2/5555555/full/#{thumbnail_size}/0/default.jpg FORGED_LOG_ENTRY")
+    end
   end
 
   context 'as an authenticated yale user' do
@@ -112,6 +136,71 @@ RSpec.describe "Iiifs", type: :request do
       get "/check-iiif", headers: { 'X-Origin-URI' => "/iiif/2/2222222/full/#{thumbnail_size}/0/default.jpg" }
 
       expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
+  context 'for Open with Permission management integration' do
+    let(:owp_user) { FactoryBot.create(:user, netid: 'owp_netid', sub: '7bd425ee-1093-40cd-ba0c-5a2355e37d6e') }
+    let(:owp_work) do
+      {
+        "id": "91818909",
+        "title_tesim": ["[Map of Australia]. [owp-only copy]"],
+        "visibility_ssi": "Open with Permission",
+        "child_oids_ssim": ["9333333"]
+      }
+    end
+
+    around do |example|
+      original_management_url = ENV['MANAGEMENT_HOST']
+      original_token = ENV['OWP_AUTH_TOKEN']
+      ENV['MANAGEMENT_HOST'] = 'http://www.example.com/management'
+      ENV['OWP_AUTH_TOKEN'] = 'valid'
+      example.run
+      ENV['MANAGEMENT_HOST'] = original_management_url
+      ENV['OWP_AUTH_TOKEN'] = original_token
+    end
+
+    before do
+      solr = Blacklight.default_index.connection
+      solr.add([owp_work])
+      solr.commit
+    end
+
+    context 'as an authenticated user for Open with Permission item' do
+      before do
+        sign_in owp_user
+        stub_request(:get, 'http://www.example.com/management/api/permission_sets/7bd425ee-1093-40cd-ba0c-5a2355e37d6e')
+          .to_return(status: 200, body: '{ "permissions": [] }', headers: {})
+        stub_request(:get, "http://www.example.com/management/api/permission_sets/91818909/#{owp_user.netid}")
+          .to_return(status: 200, body: '{ "is_admin_or_approver?": false }', headers: {})
+      end
+
+      it 'returns unauthorized and calls management APIs through mocked MANAGEMENT_HOST' do
+        get "/check-iiif", headers: { 'X-Origin-URI' => "/iiif/2/9333333/full/#{thumbnail_size}/0/default.jpg" }
+
+        expect(response).to have_http_status(:unauthorized)
+        expect(WebMock).to have_requested(:get, 'http://www.example.com/management/api/permission_sets/7bd425ee-1093-40cd-ba0c-5a2355e37d6e').at_least_once
+        expect(WebMock).to have_requested(:get, "http://www.example.com/management/api/permission_sets/91818909/#{owp_user.netid}").at_least_once
+      end
+    end
+
+    context 'as an authenticated user with approved Open with Permission access' do
+      before do
+        sign_in owp_user
+        stub_request(:get, 'http://www.example.com/management/api/permission_sets/7bd425ee-1093-40cd-ba0c-5a2355e37d6e')
+          .to_return(
+            status: 200,
+            body: '{ "permissions": [{ "oid": 91818909, "request_status": "Approved", "access_until": "2034-11-02T20:23:18.824Z" }] }',
+            headers: {}
+          )
+      end
+
+      it 'returns success when MANAGEMENT_HOST reports approved access' do
+        get "/check-iiif", headers: { 'X-Origin-URI' => "/iiif/2/9333333/full/#{thumbnail_size}/0/default.jpg" }
+
+        expect(response).to have_http_status(:success)
+        expect(WebMock).to have_requested(:get, 'http://www.example.com/management/api/permission_sets/7bd425ee-1093-40cd-ba0c-5a2355e37d6e').at_least_once
+      end
     end
   end
 end
