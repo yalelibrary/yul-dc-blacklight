a4df81f9ff4d28e23b4021c054422da29b9943c4
K8Sewell <kait@notch8.com>
2026-08-10 15:18:34 -0500

    Reorder comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,11 @@
 //= require download_original
 //= require notifications
 //= require show_more
-// blacklight_range_limit 9.x is an ES module, delivered via importmap: see
-// config/importmap.rb and app/javascript/range_limit.js
 //= require landing_hero
 //= require permission_requests_sort
 //= require grouped_metadata
+// blacklight_range_limit 9.x is an ES module, delivered via importmap: see
+// config/importmap.rb and app/javascript/range_limit.js
 
 /**
  * Setup button functionality.
