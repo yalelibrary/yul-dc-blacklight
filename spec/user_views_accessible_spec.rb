f70a2e3367244f365c053bf7d3d0b7f79a2a0dd9
JP Engstrom <jpengstrom@MacBook-Pro.local>
2026-08-28 11:41:25 -0600

    adds help guide to user accessible spec

--- spec/user_views_accessible_spec.rb
+++ spec/user_views_accessible_spec.rb
@@ -16,4 +16,10 @@ feature 'welcome', js: true do
       expect(page).to be_accessible
     end
   end
+  context 'help guide page is accessible' do
+    it 'with axe gem' do
+      visit "/help_guide"
+      expect(page).to be_accessible
+    end
+  end
 end
